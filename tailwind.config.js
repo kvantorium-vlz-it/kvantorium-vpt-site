279b9ec73cb578833a72b371386d03fa84738859
toombez <timurtokaev.toombez@gmail.com>
2022-08-26 00:28:00 +0300

    Add base tailwind constants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /**
- * @type {import('tailwindcss').Config}
+ * @type { import('tailwindcss').Config }
  */
 module.exports = {
   content: [
@@ -11,6 +11,31 @@ module.exports = {
     './nuxt.config.{js,ts}',
   ],
   theme: {
+    colors: {
+      primary: {
+        300: '#E7ECF5',
+        400: '#7689BB',
+        DEFAULT: '#234293',
+        600: '#1E397E',
+        700: '#17306F',
+        800: '#0E224A',
+      },
+      promo: '#000017',
+      white: '#FAFAFA',
+      black: '#000000',
+      auto: 'auto',
+      transparent: 'transparent',
+      inherit: 'inherit',
+    },
+    fontSize: {
+      body16: ['1rem', 1.25],
+      body20: ['1.25rem', 1.25],
+      body24: ['1.5rem', 1.25],
+      heading32: ['2rem', 1.5],
+      heading36: ['2.25rem', 1.5],
+      heading40: ['2.5rem', 1.5],
+      heading64: ['4rem', 1.5],
+    },
     extend: {},
   },
   plugins: [],
