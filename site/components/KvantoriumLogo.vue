71cedc82de612297073bdb31a585f7419d32c194
toombez <timurtokaev.toombez@gmail.com>
2022-08-26 03:04:04 +0300

    Add Logo

---
+++ site/components/KvantoriumLogo.vue
@@ -0,0 +1,13 @@
+<template>
+<img
+    src="/logo.png"
+    alt="Кванториум ВПТ"
+    :class="$style.Logo"
+/>
+</template>
+
+<style module lang="postcss">
+.Logo {
+    @apply text-primary-300 text-body24;
+}
+</style>
