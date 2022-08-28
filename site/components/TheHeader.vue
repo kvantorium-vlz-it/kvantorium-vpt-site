02ff9ebe830436ceb43a6b60c9c9cdb4540c5f26
toombez <timurtokaev.toombez@gmail.com>
2022-08-28 04:38:37 +0300

    Add TheHeader

---
+++ site/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+<header :class="$style.Header">
+    <kvantorium-logo />
+    <the-header-menu-button @click="handleMenuButtonClick"/>
+</header>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+    (e: 'menuButtonClick', event: PointerEvent): void;
+}>()
+
+const handleMenuButtonClick = (event: PointerEvent) => emit('menuButtonClick', event)
+</script>
+
+<style module lang="postcss">
+.Header {
+    @apply bg-primary-800 page-section;
+    @apply flex items-center justify-between;
+}
+</style>
