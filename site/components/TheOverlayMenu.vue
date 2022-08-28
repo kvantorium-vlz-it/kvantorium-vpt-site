55ebd8f71c7803a04afa961f931e1a8aa45a1b26
toombez <timurtokaev.toombez@gmail.com>
2022-08-29 02:34:12 +0300

    Add TheOverlay

--- site/components/TheOverlayMenu.vue
+++ site/components/TheOverlayMenu.vue
@@ -12,7 +12,7 @@
     @apply relative;
 
     &__navigation {
-        @apply overflow-auto h-full z-20;
+        @apply overflow-y-auto h-full z-20;
     }
     &__decoration {
         @apply absolute top-0 bottom-0 left-0 right-0 z-10;
