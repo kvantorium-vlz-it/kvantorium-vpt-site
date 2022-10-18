bf95dab30d6c3d91c0de10870ed55b959c76a760
toombez <timurtokaev.toombez@gmail.com>
2022-10-19 00:52:28 +0300

    Logo using postcss and css vars

--- components/KvantoriumLogo.vue
+++ components/KvantoriumLogo.vue
@@ -9,7 +9,7 @@
 </div>
 </template>
 
-<style module>
+<style module lang="postcss">
 .Logo {
     --gap: 0.5rem;
     --icon-width: calc(var(--fs-logo-top) + var(--fs-logo-bottom));
@@ -18,18 +18,20 @@
     grid-template-columns: minmax(0, var(--icon-width)) minmax(0, 1fr);
     align-items: center;
     gap: var(--gap);
-}
-.Logo__icon,
-.Logo__details {
-    color: var(--white);
-    font-weight: var(--fw-bold);
-    word-wrap: unset;
-    white-space: nowrap;
-}
-.Logo__top {
-    font-size: var(--fs-logo-top);
-}
-.Logo__bottom {
-    font-size: var(--fs-logo-bottom);
+
+    &__icon,
+    &__details {
+        color: var(--white);
+        font-weight: var(--fw-bold);
+        word-wrap: unset;
+        white-space: nowrap;
+    }
+
+    &__top {
+        font-size: var(--fs-logo-top);
+    }
+    &__bottom {
+        font-size: var(--fs-logo-bottom);
+    }
 }
 </style>
