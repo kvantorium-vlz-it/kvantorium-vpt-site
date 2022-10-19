6371f16a56ff9bfeaf90d0724599eec422a703ed
toombez <timurtokaev.toombez@gmail.com>
2022-10-19 14:50:25 +0300

    Clear TheHeader

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -3,21 +3,11 @@
     <nuxt-link to="/">
         <kvantorium-logo />
     </nuxt-link>
-    <the-header-menu-button @click="handleMenuButtonClick"/>
 </header>
 </template>
 
 <script setup lang="ts">
-const emit = defineEmits<{
-    (e: 'menuButtonClick', event: PointerEvent): void;
-}>()
-
-const handleMenuButtonClick = (event: PointerEvent) => emit('menuButtonClick', event)
 </script>
 
-<style module lang="postcss">
-.Header {
-    @apply bg-primary-800 page-section;
-    @apply flex items-center justify-between gap-2;
-}
+<style module lang="scss">
 </style>
