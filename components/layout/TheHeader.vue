58633bb374ae905c45e2eca427a93dbf9a3ea0ca
toombez <timurtokaev.toombez@gmail.com>
2022-10-20 01:36:04 +0300

    Add Header

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -1,13 +1,60 @@
+<script setup lang="ts">
+const menuToggleButton = ref()
+
+const handleCloseMobileMenu = () => {
+    isMobileNavigationOpen.value = false
+}
+
+const toggleMobileMenu = () => {
+    isMobileNavigationOpen.value = !isMobileNavigationOpen.value
+}
+
+const isMobileNavigationOpen = ref<boolean>(false)
+</script>
+
 <template>
-<header :class="$style.Header">
-    <nuxt-link to="/">
+<header :class="$style.header">
+    <nuxt-link to="/" :class="$style.header__link">
         <kvantorium-logo />
     </nuxt-link>
+
+    <the-header-navigation
+        :is-open="isMobileNavigationOpen"
+        @close="handleCloseMobileMenu"
+    />
+
+    <the-header-navigation-menu-button
+        ref="menuToggleButton"
+        :class="$style.header__navigationMenuButton"
+        @click="toggleMobileMenu"
+    />
 </header>
 </template>
 
-<script setup lang="ts">
-</script>
-
 <style module lang="scss">
+@use '@/assets/css/mixins' as *;
+@use '@/assets/css/variables' as *;
+
+.header {
+    @include page-section;
+
+    background-color: rgb(var(--bg-header));
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    &__link {
+        text-decoration: none;
+    }
+
+    &__navigation {
+
+    }
+    @media (min-width: $min-bp-desktop) {
+        &__navigationMenuButton {
+            display: none;
+        }
+    }
+}
 </style>
