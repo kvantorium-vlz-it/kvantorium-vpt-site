c991b98b81c8a55624247409bff6b5698a888e7f
toombez <timurtokaev.toombez@gmail.com>
2022-10-20 00:59:01 +0300

    Add HeaderNavigationMenuButton

---
+++ components/layout/TheHeaderNavigationMenuButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button :class="$style.button">
+        <nuxt-icon
+            name="menu"
+            :class="$style.button__icon"
+        />
+    </button>
+</template>
+
+<style module lang="scss">
+.button {
+    background: none;
+    border: none;
+    cursor: pointer;
+
+    &__icon {
+        color: rgb(var(--c-white));
+        font-size: 1.5rem;
+    }
+}
+</style>
