1416901c4327e03fe1c03c890bffa4145cb4edeb
toombez <timurtokaev.toombez@gmail.com>
2022-10-23 23:19:26 +0300

    Add TheHero section

---
+++ components/sections/TheHero.vue
@@ -0,0 +1,91 @@
+<template>
+    <section :class="$style.hero">
+        <div :class="$style.hero__wrapper">
+            <div :class="$style.hero__info">
+                <h1 :class="$style.hero__heading">
+                    «Кванториум»
+                </h1>
+                <p :class="$style.hero__paragraph">
+                    Современное учреждение дополнительного образования, оснащённое высокотехнологичным оборудованием.
+                </p>
+            </div>
+
+            <div :class="$style.hero__buttons">
+                <base-button>
+                    записаться
+                </base-button>
+
+                <base-button theme="light">
+                    узнать больше
+                </base-button>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style module lang="ts">
+</style>
+
+<style module lang="scss">
+@use '@/assets/css/mixins' as *;
+@use '@/assets/css/variables' as *;
+
+.hero {
+    --hero-image: url('@/assets/images/hero-closeup.webp');
+    --hero-image-blur: 2px;
+
+    background: url('@/assets/images/hero-closeup.webp') rgb(var(--c-black)) no-repeat bottom right;
+    background-size: contain;
+
+    &__wrapper {
+        // using page section mixin here cause image must be blurred
+        @include page-section;
+
+        backdrop-filter: blur(var(--hero-image-blur));
+    }
+
+    &__info {
+        width: min-content;
+    }
+    &__heading {
+        font-size: var(--fs-h1);
+        font-weight: var(--fw-bold);
+        margin: 0;
+        color: rgb(var(--c-secondary-100));
+
+        margin-bottom: 1rem;
+    }
+
+    &__paragraph {
+        font-size: var(--fs-body-1);
+        color: rgb(var(--c-secondary-100));
+    }
+    &__buttons {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 0.5rem;
+    }
+}
+
+@media (min-width: $min-bp-desktop) {
+    .hero {
+        --hero-image: url('@/assets/images/hero-fullshot.webp');
+        --hero-image-blur: 1px;
+
+        background-size: auto;
+
+        &__wrapper {
+            $padding-vertical: 6rem + 0.75rem;
+
+            padding-top: $padding-vertical;
+            padding-bottom: $padding-vertical;
+        }
+
+        &__buttons {
+            flex-direction: row;
+            gap: 3rem;
+        }
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,3 @@
 <template>
+    <the-hero />
 </template>
