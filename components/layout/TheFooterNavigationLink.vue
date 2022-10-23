d126f3c6d910e50d28732b0f6df39b2504519bf4
toombez <timurtokaev.toombez@gmail.com>
2022-10-23 17:37:58 +0300

    Add animation for footer nav link

--- components/layout/TheFooterNavigationLink.vue
+++ components/layout/TheFooterNavigationLink.vue
@@ -17,5 +17,13 @@ defineProps<{
 .link {
     font-size: var(--fs-body-2);
     color: rgb(var(--c-primary-100));
+    text-decoration: none;
+
+    transition: color ease-out 0.3s;
+
+    &:hover {
+        color: rgb(var(--c-primary-300));
+        text-decoration: underline;
+    }
 }
 </style>
