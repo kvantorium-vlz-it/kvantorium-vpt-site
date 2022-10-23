57d9bc9a3043e3af554b7f8b4c5a46e6405c0bbb
toombez <timurtokaev.toombez@gmail.com>
2022-10-23 17:31:44 +0300

    Add TheFooterNavigationLink

---
+++ components/layout/TheFooterNavigationLink.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+    to?: string;
+}>()
+</script>
+
+<template>
+    <nuxt-link
+        :to="to"
+        :class="$style.link"
+    >
+        <slot></slot>
+    </nuxt-link>
+</template>
+
+<style module lang="scss">
+.link {
+    font-size: var(--fs-body-2);
+    color: rgb(var(--c-primary-100));
+}
+</style>
