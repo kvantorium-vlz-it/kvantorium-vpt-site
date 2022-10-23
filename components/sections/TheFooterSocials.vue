c074b9e2b005edb2050ea4944b1f4cf5f011aabb
toombez <timurtokaev.toombez@gmail.com>
2022-10-23 17:55:35 +0300

    Add TheFooterSocials

---
+++ components/sections/TheFooterSocials.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav :class="$style.socials">
+        <the-footer-socials-provider #="{ socials }">
+            <ul :class="$style.socials__list">
+                <li v-for="(social, index) in socials" :key="index">
+                    <the-footer-social
+                        :icon="social.icon"
+                        :link="social.link"
+                    />
+                </li>
+            </ul>
+        </the-footer-socials-provider>
+    </nav>
+</template>
+
+<style module lang="scss">
+.socials {
+    &__list {
+        padding: 0;
+        list-style: none;
+    }
+}
+</style>
