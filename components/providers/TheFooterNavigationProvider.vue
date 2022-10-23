21e906f00b675b623ef0266d9582d11f416d2bbe
toombez <timurtokaev.toombez@gmail.com>
2022-10-23 17:32:08 +0300

    Add TheFooterNavigationProvider

---
+++ components/providers/TheFooterNavigationProvider.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+interface INavigationItem {
+    label: string;
+    to?: string;
+}
+
+const navigation = ref<INavigationItem[]>([])
+</script>
+
+<template>
+    <slot :navigation="navigation"></slot>
+</template>
