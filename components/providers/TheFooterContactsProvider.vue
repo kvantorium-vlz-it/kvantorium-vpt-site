19f65a16036f5610032b0cd6554bdfa78c545b59
toombez <timurtokaev.toombez@gmail.com>
2022-10-23 18:03:34 +0300

    Add TheFooterContactsProvider

---
+++ components/providers/TheFooterContactsProvider.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+type ContactType = 'link' | 'email' | 'tel' | 'geo'
+
+interface IContact {
+    type: ContactType;
+    name: string;
+    link: string;
+    label: string;
+}
+
+const contacts = ref<IContact[]>([])
+</script>
+
+<template>
+    <slot :contacts="contacts"></slot>
+</template>
