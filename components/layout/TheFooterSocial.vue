66ef49b195d6812d9918233710e8ea20d71f708b
toombez <timurtokaev.toombez@gmail.com>
2022-10-23 17:46:35 +0300

    Add TheFooterSocial

---
+++ components/layout/TheFooterSocial.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+    icon: string;
+    link: string;
+}>()
+</script>
+
+<template>
+    <a :href="link">
+        <nuxt-icon :name="icon" />
+    </a>
+</template>
+
+<style module lang="scss">
+$size: 1.5rem;
+
+.social {
+    width: $size;
+    height: $size;
+    aspect-ratio: 1;
+}
+</style>
