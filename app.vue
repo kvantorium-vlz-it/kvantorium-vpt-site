ce9efe599c68a11d7c5c28b85966210ce0851b61
toombez <timurtokaev.toombez@gmail.com>
2022-10-26 23:42:49 +0300

    Add styles for app

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
-    <div style="min-height: 100vh;">
+    <div :class="$style.app">
         <the-header />
-        <nuxt-page />
+        <main :class="$style.app__main">
+            <nuxt-page />
+        </main>
         <the-footer />
     </div>
 </template>
@@ -9,8 +11,23 @@
 <script setup lang="ts">
 useHead({
     link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
     title: 'Кванториум ВПТ'
 })
 </script>
+
+<style module lang="scss">
+.app {
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    min-height: 100vh;
+
+    &__main {
+        flex: 1 0 0;
+    }
+}
+</style>
