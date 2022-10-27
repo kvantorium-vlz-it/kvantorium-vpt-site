e16cdb80677689abf21c27336a54fd0d173d979d
toombez <timurtokaev.toombez@gmail.com>
2022-10-27 15:41:11 +0300

    Add KvantumsCardsProvider

---
+++ components/providers/KvantumsCardsProvider.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+interface ICard {
+    name: string;
+    description: string;
+    image: string;
+    link: string;
+}
+
+const cards = ref<ICard[]>([])
+</script>
+
+<template>
+    <slot :cards="cards" />
+</template>
