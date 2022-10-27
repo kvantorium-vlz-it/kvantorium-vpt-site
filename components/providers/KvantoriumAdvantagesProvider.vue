d4f4ff9f332e30017e800287a5f0d7c2806ead4c
toombez <timurtokaev.toombez@gmail.com>
2022-10-28 01:18:32 +0300

    Add test data

--- components/providers/KvantoriumAdvantagesProvider.vue
+++ components/providers/KvantoriumAdvantagesProvider.vue
@@ -5,7 +5,18 @@ interface IAdvantage {
     image: string;
 }
 
-const advantages = ref<IAdvantage[]>([])
+const advantages = ref<IAdvantage[]>([
+    {
+        description: 'Детям доступно самое современное и высокотехнологичное профессиональное оборудование, которое позволяет решать широкий спектр задач: от проектирования 3D-моделей до создания настоящих прототипов своих изобретений. Дизайн каждого помещения...',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+        title: 'Почувствовать себя в роли настоящего ученого'
+    },
+    {
+        description: 'Детям доступно самое современное и высокотехнологичное профессиональное оборудование, которое позволяет решать широкий спектр задач: от проектирования 3D-моделей до создания настоящих прототипов своих изобретений. Дизайн каждого помещения...',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+        title: 'Почувствовать себя в роли настоящего ученого'
+    },
+])
 </script>
 
 <template>
--- components/providers/KvantumsCardsProvider.vue
+++ components/providers/KvantumsCardsProvider.vue
@@ -7,7 +7,57 @@ interface ICard {
     link: string;
 }
 
-const cards = ref<ICard[]>([])
+const cards = ref<ICard[]>([
+    {
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+        link: '',
+        name: 'Lorem ipsum dolor'
+    },
+
+    {
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+        link: '',
+        name: 'Lorem ipsum dolor'
+    },
+    {
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+        link: '',
+        name: 'Lorem ipsum dolor'
+    },
+    {
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+        link: '',
+        name: 'Lorem ipsum dolor'
+    },
+    {
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+        link: '',
+        name: 'Lorem ipsum dolor'
+    },
+    {
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+        link: '',
+        name: 'Lorem ipsum dolor'
+    },
+    {
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+        link: '',
+        name: 'Lorem ipsum dolor'
+    },
+    {
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+        link: '',
+        name: 'Lorem ipsum dolor'
+    },
+])
 </script>
 
 <template>
