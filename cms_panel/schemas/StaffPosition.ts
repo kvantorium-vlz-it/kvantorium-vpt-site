523eaec9afc87051860da662ff3d2e3442f9b7a0
toombez <timurtokaev.toombez@gmail.com>
2022-10-29 02:30:40 +0300

    Add scope flags for StaffPosition

--- cms_panel/schemas/StaffPosition.ts
+++ cms_panel/schemas/StaffPosition.ts
@@ -1,5 +1,14 @@
 import { FaCog } from 'react-icons/fa'
 
+const POSITION_FLAGS = [
+    {
+        name: 'isTeacher',
+        title: 'преподаватель',
+        type: 'boolean',
+        initialValue: false,
+    },
+]
+
 export default {
     name: 'StaffPosition',
     title: 'Должности сотрудников',
@@ -9,6 +18,13 @@ export default {
             name: 'name',
             title: 'Название должность',
             type: 'string',
+        },
+        {
+            name: 'scopes',
+            title: 'Возможности',
+            type: 'object',
+            description: 'Информация о том, что позволяет делать должность. Используется внутри сайта чтобы, например, отобразить только всех преподавателей кванториума.',
+            fields: POSITION_FLAGS,
         },
     ],
     icon: FaCog,
