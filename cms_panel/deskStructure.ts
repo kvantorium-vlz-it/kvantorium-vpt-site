079d7c93a883634dcd4bb03ba3192dac6d6c66a9
toombez <timurtokaev.toombez@gmail.com>
2022-10-30 05:08:17 +0300

    Setup desk

--- cms_panel/deskStructure.ts
+++ cms_panel/deskStructure.ts
@@ -1,10 +1,14 @@
 import S from '@sanity/base/structure-builder'
-import { FaCogs, FaFolder } from 'react-icons/fa'
+import { FaCogs, FaFile, FaFolder, FaPhoneAlt, FaVk } from 'react-icons/fa'
 
 import SiteSettings from './schemas/static/SiteSettings'
 
 import Icon from './schemas/documents/Icon'
 import IndexPage from './schemas/static/IndexPage'
+import StaffPosition from './schemas/documents/StaffPosition'
+import Document from './schemas/documents/Document'
+import Contact from './schemas/documents/Contact'
+import SocialNetwork from './schemas/documents/SocialNetwork'
 
 const staticDocuments = [
     SiteSettings.name,
@@ -14,6 +18,10 @@ const staticDocuments = [
 const excludedDocuments = [
     ...staticDocuments,
     Icon.name,
+    StaffPosition.name,
+    Document.name,
+    Contact.name,
+    SocialNetwork.name,
 ]
 
 export default () =>
@@ -25,9 +33,27 @@ export default () =>
                 .filter(item => !excludedDocuments.includes(item.getId())),
             S.divider(),
             S.documentTypeListItems()
+                .find(item => item.getId() === Document.name)
+                .icon(FaFile)
+                .title('Документы'),
+            S.documentTypeListItems()
+                .find(item => item.getId() === Contact.name)
+                .icon(FaPhoneAlt)
+                .title('Контакты'),
+            S.documentTypeListItems()
+                .find(item => item.getId() === SocialNetwork.name)
+                .icon(FaVk)
+                .title('Социальные сети'),
+            S.divider(),
+            S.documentTypeListItems()
+                .find(item => item.getId() === StaffPosition.name)
+                .icon(FaFolder)
+                .title('Должности'),
+            S.documentTypeListItems()
                 .find(item => item.getId() === Icon.name)
                 .icon(FaFolder)
                 .title('Иконки'),
+            S.divider(),
             S.listItem()
                 .title('Главная страница')
                 .id(`${IndexPage.name}Item`)
