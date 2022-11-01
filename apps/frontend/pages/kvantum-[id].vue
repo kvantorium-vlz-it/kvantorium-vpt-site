19d250e1efb38d41836bb94dd734bebb59e243c0
Timur <timurtokaev.toombez@gmail.com>
2022-11-01 20:12:46 +0300

    Add store for kvantums

---
+++ apps/frontend/pages/kvantum-[id].vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useKvantumsStore } from '~~/store/kvamtuns'
+
+const kvantumsStore = storeToRefs(useKvantumsStore())
+const kvantum = kvantumsStore.map.value.get(useRoute().params.id as string)
+</script>
+
+<template>
+    <page-section as="main">
+        <page-section-content>
+            <client-only>
+                <template #heading>
+                    {{ kvantum.name || 'Название квантума' }}
+                </template>
+                <rich-text :blocks="kvantum.description" />
+            </client-only>
+            <base-button>
+                записаться
+            </base-button>
+        </page-section-content>
+    </page-section>
+</template>
