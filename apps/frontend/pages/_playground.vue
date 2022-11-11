75b5864fcc07a6d0d7365045c06c878656a3f5bf
Timur <timurtokaev.toombez@gmail.com>
2022-11-11 12:33:54 +0300

    Add playground page

---
+++ apps/frontend/pages/_playground.vue
@@ -0,0 +1,13 @@
+<template>
+    <div :class="$style.playground">
+        playground
+    </div>
+</template>
+
+<style module lang="scss">
+.playground {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
