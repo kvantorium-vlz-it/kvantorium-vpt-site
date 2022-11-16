f575767684a8ae49196c45dfe3875ffba5f4f070
Timur <timurtokaev.toombez@gmail.com>
2022-11-16 18:56:39 +0300

    Add KvantoriumLocationMap

---
+++ apps/frontend/components/KvantoriumLocationMap.vue
@@ -0,0 +1,50 @@
+<script setup>
+import ymaps from 'ymaps'
+
+const YANDEX_API = '20dfd6f4-545a-43cf-b1c8-96e82339316a'
+
+const KVANTORIUM_LOCATION = {
+    latitude: 48.786934,
+    longitude: 44.772160,
+}
+
+onMounted(() => {
+    ymaps
+        .load(`https://api-maps.yandex.ru/2.1/?apikey=${YANDEX_API}&lang=ru_RU`)
+        .then(maps => {
+            const map = new maps.Map('map', {
+                center: [KVANTORIUM_LOCATION.latitude, KVANTORIUM_LOCATION.longitude],
+                zoom: 15
+            })
+
+            map.controls.remove('trafficControl')
+            map.controls.remove('searchControl')
+            map.controls.remove('typeSelector')
+
+            map.geoObjects.add(new maps.Placemark([KVANTORIUM_LOCATION.latitude, KVANTORIUM_LOCATION.longitude], {
+                balloonContent: '<strong>Кванториум ВОЛЖСКИЙ ПОЛИТЕХ</strong>'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: '/images/logo.png',
+                iconImageSize: [32, 32],
+
+                preset: 'islands#icon',
+                iconColor: '#0095b6',
+            }))
+        })
+        .catch(error => console.log('Failed to load Yandex Maps', error));
+})
+</script>
+
+<template>
+    <div id="map" :class="$style.map"></div>
+</template>
+
+<style module lang="scss">
+.map {
+    width: 100%;
+    box-shadow: var(--bs-8);
+    border-radius: var(--br-16);
+    overflow: hidden;
+}
+</style>
