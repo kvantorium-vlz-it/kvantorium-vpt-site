deba70b983bf525bb089d018cdd125378887d209
Timur <timurtokaev.toombez@gmail.com>
2022-11-16 15:54:42 +0300

    Add base styles for app

--- apps/frontend/app.vue
+++ apps/frontend/app.vue
@@ -1,6 +1,20 @@
 <template>
-    <TheHeader />
-    <div>
-        <NuxtPage />
+    <div :class="$style.app">
+        <TheHeader />
+        <div>
+            <NuxtPage />
+        </div>
+        <TheFooter />
     </div>
 </template>
+
+<style module lang="scss">
+.app {
+    overflow-x: clip;
+    min-height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+</style>
