efaa9ccffa126865ec16f1414ea1e3b3b8b72c5e
Timur <timurtokaev.toombez@gmail.com>
2023-06-03 02:38:56 +0300

    Add advantages

---
+++ apps/frontend/components/Advantage.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{
+    icon: string
+    heading: string
+    description: string
+}>()
+</script>
+
+<template>
+    <article
+        class="clear-both"
+        :title="heading"
+    >
+        <img
+            class="w-48 float-left mr-4 mb-4"
+            :src="icon"
+            :alt="heading"
+        >
+
+        <h3 class="mb-4 text-[24px] font-medium">
+            {{ heading }}
+        </h3>
+
+        <div class="text-[22px]">
+            {{ description }}
+        </div>
+    </article>
+</template>
