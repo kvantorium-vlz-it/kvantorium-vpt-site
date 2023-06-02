752cddd5e5677ef8e563a382cf14acf641dcf8f7
Timur <timurtokaev.toombez@gmail.com>
2023-06-02 23:41:10 +0300

    Add Container

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -10,6 +10,16 @@ export default {
         "./app.vue",
     ],
     theme: {
+        screens: {
+            'desktop': '1720px'
+        },
+        container: {
+            center: true,
+            padding: {
+                'DEFAULT': '0.5rem',
+                'desktop': '0.75rem',
+            },
+        },
         extend: {},
     },
     plugins: [],
