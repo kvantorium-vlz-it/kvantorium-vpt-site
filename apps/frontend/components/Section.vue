f08157190561ba25e5663457eec269047a194f4e
Timur <timurtokaev.toombez@gmail.com>
2023-06-03 03:05:31 +0300

    Add Section

---
+++ apps/frontend/components/Section.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+    heading: string
+    id?: string
+}>()
+</script>
+
+<template>
+    <section
+        class="py-12"
+        :id="id"
+    >
+        <Container>
+            <h2 class="mb-14 text-center mx-auto text-[#3C4580] font-black text-[38px]">
+                {{ heading }}
+            </h2>
+
+            <slot></slot>
+        </Container>
+    </section>
+</template>
