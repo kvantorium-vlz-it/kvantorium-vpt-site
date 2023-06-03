624d1c82e63bf9830b27c850b1decbeaa3a5f400
Timur <timurtokaev.toombez@gmail.com>
2023-06-03 03:30:24 +0300

    Add TheDemoLessons

---
+++ apps/frontend/components/TheDemoLessons.vue
@@ -0,0 +1,46 @@
+<template>
+    <Section heading="Демонстрационные занятия июнь 2023">
+        <ul class="grid grid-cols-3 gap-x-24 gap-y-8">
+            <li>
+                <DemoLessonCard
+                    :age="{ from: 12, to: 18 }"
+                    image="/images/hero-closeup.webp"
+                    kvantum="IT-Квантум"
+                    :time="{ from: '10:00', to: '11:30' }"
+                    name="Терменвокс"
+                    teacher="Золотухин В.Р."
+                />
+            </li>
+            <li>
+                <DemoLessonCard
+                    :age="{ from: 12, to: 18 }"
+                    image="/images/hero-closeup.webp"
+                    kvantum="IT-Квантум"
+                    :time="{ from: '10:00', to: '11:30' }"
+                    name="Терменвокс"
+                    teacher="Золотухин В.Р."
+                />
+            </li>
+            <li>
+                <DemoLessonCard
+                    :age="{ from: 12, to: 18 }"
+                    image="/images/hero-closeup.webp"
+                    kvantum="IT-Квантум"
+                    :time="{ from: '10:00', to: '11:30' }"
+                    name="Терменвокс"
+                    teacher="Золотухин В.Р."
+                />
+            </li>
+            <li>
+                <DemoLessonCard
+                    :age="{ from: 12, to: 18 }"
+                    image="/images/hero-closeup.webp"
+                    kvantum="IT-Квантум"
+                    :time="{ from: '10:00', to: '11:30' }"
+                    name="Терменвокс"
+                    teacher="Золотухин В.Р."
+                />
+            </li>
+        </ul>
+    </Section>
+</template>
