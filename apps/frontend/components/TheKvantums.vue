0d87293444e68d3e1bd8f88a01c0c9b15789ce17
Timur <timurtokaev.toombez@gmail.com>
2023-06-04 18:37:41 +0300

    Redesign kvantums section

--- apps/frontend/components/TheKvantums.vue
+++ apps/frontend/components/TheKvantums.vue
@@ -1,45 +1,17 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-import { useKvantumsStore } from '@/store/kvantums'
+import { useKvantumsStore } from '~/store/kvantums';
 
-interface IKvantum {
-    name: string
-    description: string
-    image: string
-    to?: string
-}
+const kvantums = await useKvantumsStore().fetchAllKvantuns()
 
-const kvantumsStore = storeToRefs(useKvantumsStore())
-
-const kvantums = computed<IKvantum[]>(() => {
-    return kvantumsStore.kvantums.value?.map(kvantum => ({
-        name: kvantum.name,
-        description: kvantum.shortDescription,
-        image: kvantum.icon,
-        to: `/kvantum-${kvantum.id}`,
-    })) || []
+const mainKvantums = computed(() => {
+    return kvantums.value.filter((kvantum) => !kvantum.isAdditional)
 })
-
-const breakpoints = useBreakpoints({
-    desktop: 1024,
-})
-const isDesktop = breakpoints.greater('desktop')
-
-const slidesInRow = computed(() => isDesktop.value ? 2 : 1)
 </script>
 
 <template>
-    <PageSection>
-        <PageSectionDefaultLayout>
-            <template #heading>
-                Квантумы
-            </template>
-            <template #subheading>
-                Программы Кванториума предназначены для школьников с 5 по 11 класс
-            </template>
-            <ClientOnly>
-                <KvantumsSwiper :kvantums="kvantums" :items-in-row="slidesInRow" />
-            </ClientOnly>
-        </PageSectionDefaultLayout>
-    </PageSection>
+    <Section heading="Основные направления">
+        <KvantumSlider
+            :kvantums="mainKvantums"
+        />
+    </Section>
 </template>
