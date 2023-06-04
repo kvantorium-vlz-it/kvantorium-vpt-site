1efc741bc4a0ab293a88c7e2aa7dd2e96c3e1a4d
Timur <timurtokaev.toombez@gmail.com>
2023-06-04 18:48:53 +0300

    Add FooterSocialLink

---
+++ apps/frontend/components/FooterSocialLink.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+    icon: string
+    label: string
+    href: string
+}>()
+</script>
+
+<template>
+    <NuxtLink
+        :to="href"
+        class="text-black text-[30px] flex items-center gap-1 font-bold"
+    >
+        <Icon :name="icon" class="text-[50px]" />
+        {{ label }}
+    </NuxtLink>
+</template>
