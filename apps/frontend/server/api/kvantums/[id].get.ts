42b78ef7890b945e8510add007a6a309c5a0ca85
Timur <timurtokaev.toombez@gmail.com>
2023-06-07 00:38:58 +0300

    Fix type into kvantum/:id

--- apps/frontend/server/api/kvantums/[id].get.ts
+++ apps/frontend/server/api/kvantums/[id].get.ts
@@ -1,8 +1,15 @@
-import { Kvantum } from '@kvantorium-vpt-site/sanity-schema'
+import { Kvantum } from '@/assets/typescript/types'
 
 export default defineEventHandler(async (event) => {
     const id = getQuery(event).id
-    const query = groq`*[_type == 'kvantum' && _id == ${id}]`
+    const query = groq`*[_type == 'kvantum' && _id == ${id}] {
+        'icon': icon.asset-> url,
+        'promoImage': promoImage.asset -> url,
+        description,
+        name,
+        isAdditional,
+        'id': _id,
+    }`
 
     return await useSanity().fetch<Kvantum | undefined>(query)
 })
