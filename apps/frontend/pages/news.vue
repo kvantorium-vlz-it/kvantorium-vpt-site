e7ad44ae5fa333b52f7b25b33985853384a7ed7d
Timur <timurtokaev.toombez@gmail.com>
2023-06-07 01:52:01 +0300

    Refactor news page

--- apps/frontend/pages/news.vue
+++ apps/frontend/pages/news.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-import { useNewsStore } from '@/store/News'
+import { useNewsStore } from '@/store/news'
 
 const store = storeToRefs(useNewsStore())
+const news = store.news
 </script>
 
 <template>
-    <PageSection
-        v-for="(news) in store.news.value"
-        :key="news.id"
-    >
-        <PageSectionDefaultLayout>
-            <template #heading>
-                {{ news.name }}
-            </template>
-
-            <RichText :blocks="news.content"></RichText>
-        </PageSectionDefaultLayout>
-    </PageSection>
+    <Section heading="Новости">
+        <ul class="flex flex-col gap-[20px]">
+            <li
+                v-for="post in news"
+                :key="post.id"
+            >
+                <NewsPost
+                    :news="post"
+                />
+            </li>
+        </ul>
+    </Section>
 </template>
