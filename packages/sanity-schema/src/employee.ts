ebbd03bd5322d93d93ed23a8d5030c6e7707d90d
Timur <timurtokaev.toombez@gmail.com>
2023-06-07 00:26:55 +0300

    Add employee

---
+++ packages/sanity-schema/src/employee.ts
@@ -0,0 +1,24 @@
+import { s } from '@sanity-typed/schema-builder'
+
+export const employee = s.document({
+    name: 'employee',
+    title: 'Сотрудники',
+    fields: [
+        {
+            name: 'firstName',
+            title: 'Имя',
+            type: s.string(),
+        },
+        {
+            name: 'secondName',
+            title: 'Фамилия',
+            type: s.string(),
+        },
+        {
+            name: 'middleName',
+            title: 'Отчество',
+            type: s.string({}),
+            optional: true,
+        },
+    ],
+})
