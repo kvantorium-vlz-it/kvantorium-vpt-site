35ac432dc356f6c0c5f8a9c5b4aa0d35b9b8c947
Timur <timurtokaev.toombez@gmail.com>
2023-06-07 20:53:48 +0300

    Add SignUpModalInfo

---
+++ apps/frontend/components/SignUpModalInfo.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+    icon: string
+    title: string
+    href: string
+    label: string
+}>()
+</script>
+
+<template>
+    <div
+        class="flex gap-[20px] items-center"
+    >
+        <Icon
+            :name="icon"
+            class="aspect-square text-black text-[100px]"
+        />
+
+        <address
+            class="text-black not-italic text-[30px] font-medium"
+        >
+            {{ title }}
+
+            <a :href="href">
+                {{ label }}
+            </a>
+        </address>
+    </div>
+</template>
