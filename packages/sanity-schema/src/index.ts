5fb95f7b9915f793f246a3eb5deac0ece181e0f0
Timur <timurtokaev.toombez@gmail.com>
2023-06-07 15:11:24 +0300

    Remove unused schemas

--- packages/sanity-schema/src/index.ts
+++ packages/sanity-schema/src/index.ts
@@ -1,26 +1,17 @@
-import { staffPosition, StaffPosition } from './staffPosition'
 import { news, RawNews } from './news'
 import { kvantum, RawKvantum } from './kvantum'
-import { file, File } from './file'
-import { staff, Staff } from './staff'
 import { curriculum, RawCurriculum } from './curriculum'
 import { employee, RawEmployee } from './employee'
 
 export type {
-    StaffPosition,
     RawNews,
     RawKvantum,
-    File,
-    Staff,
-    RawEmployee,
     RawCurriculum,
+    RawEmployee,
 }
 export {
-    staffPosition,
     news,
     kvantum,
-    file,
-    staff,
     curriculum,
     employee,
 }
