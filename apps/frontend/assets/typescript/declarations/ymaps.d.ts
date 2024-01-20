4477b12a030ae38fa1a4f6c6260d6cfd6f598d78
Timur <toombez.timurtokaev@gmail.com>
2024-01-20 22:12:08 +0300

    Clear repo for v3

--- apps/frontend/assets/typescript/declarations/ymaps.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'ymaps' {
-    declare function load(...args: any[]): Promise<any>
-}
--- apps/frontend/pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <TheHero id="hero" />
-    <LazyTheAdvantages id="advantages" />
-    <LazyTheKvantums id="kvatums" />
-    <TheDemoLessons id="demo-lessons" />
-</template>
