4793221cc283f7e8a9379caf4c06d729e122ade7
Timur <toombez.timurtokaev@gmail.com>
2024-02-14 23:40:44 +0300

    Add AllComponentProps type

--- apps/web/composables/useGlobalCursor.ts
+++ apps/web/composables/useGlobalCursor.ts
@@ -1,6 +1,16 @@
 import { createGlobalState } from '@vueuse/core'
+import type { AllowedComponentProps, VNodeProps } from 'vue'
 
 export type ComponentProps<
+    C extends Component
+> = C extends new (...args: any) => any
+    ? Omit<
+        InstanceType<C>['$props'],
+        keyof VNodeProps | keyof AllowedComponentProps
+    >
+    : never
+
+export type AllComponentProps<
     C extends Component
 > = C extends new (...args: any) => any
     ? InstanceType<C>['$props']
@@ -10,7 +20,7 @@ function useCursorGlobalPrivate<
     _C extends Component
 >() {
     const _CursorComponent = shallowRef<_C | null>(null)
-    const _attrs = shallowRef<ComponentProps<_C> | null>(null)
+    const _attrs = shallowRef<AllComponentProps<_C> | null>(null)
 
     return {
         Cursor: readonly(_CursorComponent),
