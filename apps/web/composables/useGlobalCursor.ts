5a113ba9c01cbbb04c1e4b5f9509ba81fd31b75a
Timur <toombez.timurtokaev@gmail.com>
2024-02-15 00:54:29 +0300

    Use useDynamicComponent

--- apps/web/composables/useGlobalCursor.ts
+++ apps/web/composables/useGlobalCursor.ts
@@ -1,78 +1,33 @@
 import { createGlobalState } from '@vueuse/core'
 import type { AllowedComponentProps, Component, ShallowRef, VNodeProps } from 'vue'
 
-export type ComponentProps<
-    C extends Component
-> = C extends new (...args: any) => any
-    ? Omit<
-        InstanceType<C>['$props'],
-        keyof VNodeProps | keyof AllowedComponentProps
-    >
-    : never
-
-export type AllComponentProps<
-    C extends Component
-> = C extends new (...args: any) => any
-    ? InstanceType<C>['$props']
-    : never
+interface ComponentOptions<C extends Component = Component> {
+    Component: C
+    attrs: ComponentProps<C>
+}
 
 function useCursorGlobalPrivate<
     _C extends Component
 >() {
-    const _CursorComponent = shallowRef<_C | null>(null)
-    const _cursorAttrs = shallowRef<AllComponentProps<_C> | null>(null)
-
-    const _TransitionComponent = shallowRef<Component | null>(null)
-    const _transitionAttrs = shallowRef<AllComponentProps<_C> | null>(null)
+    const cursor = useDynamicComponent()
+    const cursorTransition = useDynamicComponent()
 
     const _isVisibleGlobalCursor = ref<boolean>(true)
-
-    function setComponent<C extends Component>(
-        target: ShallowRef<Component | null>,
-        targetAttrs: ShallowRef<AllComponentProps<C> | null>,
-        newComponent: Component | null,
-        newAttrs: AllComponentProps<C> | null,
-    ) {
-        target.value = newComponent
-        targetAttrs.value = newAttrs
-    }
-
-    function setCursorComponent<C extends Component = _C>(
-        component: C | null = null,
-        attrs: AllComponentProps<C> | null = null,
-    ) {
-        setComponent(_CursorComponent, _cursorAttrs, component, attrs)
-    }
-
-    function clearCursor() {
-        setCursorComponent()
-    }
-
-    function setTrasitionComponent<C extends Component = _C>(
-        component: C | null = null,
-        attrs: AllComponentProps<C> | null = null,
-    ) {
-        setComponent(_TransitionComponent, _transitionAttrs, component, attrs)
-    }
-
-    function clearTransition() {
-        setTrasitionComponent()
-    }
 
     function toggleCursor(isVisible = !_isVisibleGlobalCursor.value) {
         _isVisibleGlobalCursor.value = isVisible
     }
 
     return {
-        Cursor: shallowReadonly(_CursorComponent),
-        cursorAttrs: readonly(_cursorAttrs),
-        TransitionComponent: shallowReadonly(_TransitionComponent),
-        transitionAttrs: readonly(_transitionAttrs),
+        Cursor: cursor.Component,
+        cursorAttrs: cursor.attrs,
+        TransitionComponent: cursorTransition.Component,
+        transitionAttrs: cursorTransition.attrs,
         isVisibleGlobalCursor: readonly(_isVisibleGlobalCursor),
-        setCursorComponent,
-        clearCursor,
-        setTrasitionComponent,
-        clearTransition,
+        setCursorComponent: cursor.setComponent,
+        clearCursor: cursor.clearComponent,
+        setTrasitionComponent: cursorTransition.setComponent,
+        clearTransition: cursorTransition.clearComponent,
         toggleCursor,
     }
 }
