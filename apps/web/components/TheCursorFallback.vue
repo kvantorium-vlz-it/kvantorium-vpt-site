91c3f9306efa1f1029400ba3488f78cc37dcb32b
Timur <81884993+toombez@users.noreply.github.com>
2024-02-15 16:39:17 +0300

    Feat/custom-cursor
    * Create vue-dynamic-cursor package
    
    * Add vueUse
    
    * Add ComponentProps type
    
    * Store Component and attrs in shared composable
    
    * Add AllComponentProps type
    
    * Add functions for manage state
    
    * Add flag for hide cursor
    
    * Add TransitionComponent
    
    * Add TheCursorFallback
    
    * Add TheCursor
    
    * Add pnp-lock
    
    * Add useDynamicComponent
    
    * Use useDynamicComponent
    
    * Change API and rename
    
    * Render fallbackTransition
    
    * Add ChangeCursor
    
    * Delete commentary from TheCursor
    
    * Delete package

---
+++ apps/web/components/TheCursorFallback.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="$style.dot"></div>
+</template>
+
+<style module>
+.dot {
+    position: absolute;
+    --circle-size: 40px;
+    min-height: var(--circle-size);
+    min-width: var(--circle-size);
+    width: fit-content;
+    height: fit-content;
+    border: 1px solid black;
+    border-radius: 100%;
+    top: calc(var(--circle-size) / 2 * -1);
+    left: calc(var(--circle-size) / 2 * -1);
+}
+</style>
