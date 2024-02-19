f98c2373e3b6514a97ab64aa443ac4aa4d9017c7
Timur <toombez.timurtokaev@gmail.com>
2024-02-19 13:57:44 +0300

    Add TheHeroButton

---
+++ apps/web/components/TheHeroButton.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import type { RouterLinkProps } from 'vue-router';
+
+type Props = Omit<RouterLinkProps, 'to'> & {
+    to?: RouterLinkProps['to']
+}
+
+defineProps<Props>()
+
+const button = ref<HTMLAnchorElement | null>(null)
+
+const {
+    roll,
+    tilt,
+} = useParallax(button)
+const { toggleCursor } = useGlobalCursor()
+const { elementWidth: width, elementHeight: height } = useMouseInElement(button)
+
+const iconX = computed(() => tilt.value * width.value / 4)
+const iconY = computed(() => -roll.value * height.value / 4)
+
+const iconWrapperTranslate = computed(() => {
+    return `translate: ${iconX.value}px ${iconY.value}px;`
+})
+
+const iconWrapperRotate = computed(() => {
+    const hypot = Math.hypot(deltaYToBottom.value, deltaXToCenter.value)
+    const cosBetweenLines = deltaYToBottom.value / hypot
+    const sign = Math.sign(deltaXToCenter.value)
+    const angle = sign * Math.acos(cosBetweenLines) * 90
+
+    return `rotate: ${angle}deg;`
+})
+const deltaYToBottom = computed(() => height.value / 2 - iconY.value)
+const deltaXToCenter = computed(() => iconX.value)
+</script>
+
+<template>
+    <NuxtLink
+        ref="button"
+        @mouseover="() => toggleCursor(false)"
+        @mouseout="() => toggleCursor(true)"
+        :to="to"
+        :class="$style.button"
+    >
+        <!-- <div style="position: fixed; top: 50px; left: 50px; color: white;">
+            icon: {{ iconX }}|{{ iconY }}<br>
+            wrapper: {{ width }}|{{ height }}<br>
+            deltas: {{ deltaXToCenter }}|{{ deltaYToBottom }}
+        </div> -->
+        <div
+            :class="$style['icon-wrapper']"
+            :style="[iconWrapperTranslate, iconWrapperRotate]"
+        >
+            <Icon
+                name="ph:arrow-down"
+                :class="$style.icon"
+                size="100%"
+            />
+        </div>
+    </NuxtLink>
+</template>
+
+<style module>
+.button {
+    position: relative;
+
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    aspect-ratio: 1;
+    border-radius: 100vw;
+    width: 4rem;
+
+    transition: all 0.1s ease-out;
+}
+.button:hover {
+    width: 18rem;
+    padding: 7rem;
+}
+.button::after {
+    content: '';
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    background-color: hsla(0, 0%, 0%, 0.6);
+    mix-blend-mode: overlay;
+
+    border-radius: 100vw;
+    border: 2px solid var(--c-site-background);
+}
+.button .icon-wrapper {
+    display: none;
+}
+.button:hover .icon-wrapper {
+    display: flex;
+}
+
+.icon {
+    width: 100%;
+    width: 2.5rem;
+    aspect-ratio: 1;
+    color: var(--c-site-background);
+}
+.icon-wrapper {
+    padding: 1rem;
+    border: 2px solid var(--c-site-background);
+    border-radius: 100vw;
+    width: fit-content;
+    aspect-ratio: 1;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
+</style>
