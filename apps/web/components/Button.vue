7564f24044635d88d4fe5747338432e6d96b8f48
Timur <toombez.timurtokaev@gmail.com>
2024-02-25 17:02:59 +0300

    Use outline and variables for Button

--- apps/web/components/Button.vue
+++ apps/web/components/Button.vue
@@ -24,33 +24,47 @@ withDefaults(defineProps<Props>(), {
 
 <style module>
 .button {
-    text-decoration: none;
-    font-family: 'Circe';
-    font-size: 1rem;
-    padding: 0.25rem 2rem;
+    --background-color: transparent;
+    --gradient-left-color: transparent;
+    --gradient-right-color: transparent;
+    --border-color: transparent;
+    --text-color: var(--c-site-background);
+
+    --_border-width: 0.125rem;
+
     display: inline-flex;
-    border-radius: 100vw;
     justify-content: center;
     align-items: center;
     gap: 0.5rem;
-    color: var(--c-site-background);
+    padding: 0.25rem 2rem;
+
+    outline-color: var(--border-color);
+    outline-width: var(--_border-width);
+    outline-offset: calc(var(--_border-width) * -1);
+    outline-style: solid;
+    border-radius: 100vw;
+
+    color: var(--text-color);
+    font-family: 'Circe';
+    font-size: 1rem;
+    text-decoration: none;
+
+    background-color: var(--background-color);
+    background-image: linear-gradient(
+        90deg,
+        var(--gradient-left-color),
+        var(--gradient-right-color)
+    );
 }
 .primary {
-    background: linear-gradient(
-        90deg,
-        var(--c-site-primary),
-        var(--c-site-secondary-darker-1)
-    );
+    --gradient-left-color: var(--c-site-primary);
+    --gradient-right-color: var(--c-site-secondary-darker-1);
 }
 .secondary {
-    background: linear-gradient(
-        90deg,
-        var(--c-site-secondary),
-        var(--c-site-secondary-lighter-1)
-    );
+    --gradient-left-color: var(--c-site-secondary);
+    --gradient-right-color: var(--c-site-secondary-lighter-1);
 }
 .white {
-    border: 2px solid var(--c-site-background);
-    background: transparent;
+    --border-color: var(--c-site-background);
 }
 </style>
