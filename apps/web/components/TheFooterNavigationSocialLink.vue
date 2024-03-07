34b4502eb780b36238bd1986e89c9141b33718af
Timur <toombez.timurtokaev@gmail.com>
2024-03-07 17:59:18 +0300

    Add components for navigation

---
+++ apps/web/components/TheFooterNavigationSocialLink.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { RouterLinkProps } from 'vue-router';
+
+interface Props extends Omit<RouterLinkProps, 'to'> {
+    icon: string
+    color: string
+    to?: RouterLinkProps['to']
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    color: '#000000',
+})
+
+const colorStyles = computed(() => `color: ${props.color};`)
+</script>
+
+<template>
+    <NuxtLink
+        :="$props"
+        :class="$style.link"
+    >
+        <Icon
+            :name="icon"
+            :style="colorStyles"
+            :class="$style.icon"
+        />
+    </NuxtLink>
+</template>
+
+<style module>
+.link {
+    display: inline-flex;
+    aspect-ratio: 1;
+    padding: 0.25rem;
+    background-color: var(--c-site-background);
+    border-radius: 100vw;
+}
+.icon {
+    font-size: 1rem;
+}
+</style>
