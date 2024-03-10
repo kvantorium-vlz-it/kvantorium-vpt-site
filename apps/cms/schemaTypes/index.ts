7ed2ecbe41eb77bd8298e2f56ab2022bbc8e3b9f
Timur <toombez.timurtokaev@gmail.com>
2024-03-10 23:21:41 +0300

    Use schemas in cms

--- apps/cms/schemaTypes/index.ts
+++ apps/cms/schemaTypes/index.ts
@@ -1,7 +1,11 @@
 import employee from "./employee"
+import frequentlyAskedQuestions from "./frequentlyAskedQuestions"
 import kvantum from "./kvantum"
+import news from "./news"
 
 export const schemaTypes = [
-    kvantum,
     employee,
+    frequentlyAskedQuestions,
+    kvantum,
+    news,
 ]
