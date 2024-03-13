0e9e7ffdb6a1ae5828a11781caeb0bb135aec4d4
Timur <toombez.timurtokaev@gmail.com>
2024-03-14 00:34:41 +0300

    Add Accordion

---
+++ packages/ui/src/runtime/components/ui/AccordionItem.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+interface Props {
+    label: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <details :class="$style['accordion-item']">
+        <summary :class="$style.trigger">
+            {{ label }}
+
+            <Icon
+                name="ph:caret-down"
+                :class="$style['trigger-icon']"
+            />
+        </summary>
+
+        <div :class="$style.content">
+            <slot></slot>
+        </div>
+    </details>
+</template>
+
+<style module>
+/* Base styles */
+.accordion-item {
+    /* Variables */
+    --text-color: var(--c-site-text-lighter-2);
+    --icon-color: var(--c-site-text);
+
+    /* Private variables */
+
+    --_border-width: 2px;
+    --_padding-inline: 1rem;
+
+    border-image: linear-gradient(
+        to right,
+        transparent,
+        var(--c-site-background-darker-2),
+        transparent
+    );
+    border-image-slice: 30;
+    border-bottom: var(--_border-width) solid;
+    border-top: var(--_border-width) solid;
+
+    font-family: 'Circe';
+    font-size: 1rem;
+    text-align: center;
+
+    padding-block: 0.75rem;
+}
+.trigger {
+    list-style: none;
+    cursor: pointer;
+
+    color: var(--text-color);
+
+    position: relative;
+    padding-inline: calc(var(--_padding-inline) + 1em + 1ch);
+}
+.trigger-icon {
+    position: absolute;
+    top: 50%;
+    right: var(--_padding-inline);
+    translate: 0 -50%;
+
+    color: var(--icon-color);
+
+    transition: rotate 0.2s ease;
+}
+.accordion-item[open] .trigger-icon {
+    rotate: 180deg;
+}
+
+.content {
+    margin-top: 1rem;
+    padding-inline: calc(var(--_padding-inline) + 1em + 1ch);
+}
+</style>
