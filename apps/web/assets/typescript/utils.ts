fa9ca2ea5a9f94f3b8dfa5064e6996889d6b164f
Timur <toombez.timurtokaev@gmail.com>
2024-03-15 13:40:13 +0300

    Fix TheKvantums Section

--- apps/web/assets/typescript/utils.ts
+++
@@ -1,7 +0,0 @@
-export function modulusLoop(index: number, length: number, delta: number) {
-    return (index + length + delta) % length
-}
-
-export function clamp(value: number, min: number, max: number) {
-    return Math.max(Math.min(value, max), min)
-}
