9f512e399a6e8523c8c6ff9d441c6f13cf30e6cc
Timur <toombez.timurtokaev@gmail.com>
2024-03-16 12:25:55 +0300

    Add ContentBlock for news

---
+++ apps/web/components/SanityImageBlock.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+interface Props {
+    image: string
+    alt: string
+    isFloating: boolean
+    floatingSide?: 'left' | 'right' | null
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <img
+        :src="image"
+        :alt="alt"
+        :class="[
+            $style.image,
+            isFloating ? $style[floatingSide!] : null,
+            { [$style.floating]: isFloating },
+        ]"
+    >
+</template>
+
+<style module>
+.image {
+    display: block;
+    object-fit: cover;
+    width: 100%;
+    margin-top: 1em;
+    margin-bottom: 1em;
+}
+.floating {
+    width: 40%;
+    float: var(--floating);
+    margin-top: 0.25em;
+    margin-bottom: 0.25em;
+}
+.floating::after {
+    content: "";
+    clear: both;
+    display: table;
+}
+.floating::before {
+    content: "";
+    clear: both;
+    display: table;
+}
+.left {
+    --floating: left;
+    margin-right: 1ch;
+}
+.right {
+    --floating: right;
+}
+</style>
