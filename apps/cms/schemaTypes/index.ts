0bfa49d8e55862d91c8c48765dfeb84461ae3252
Timur <toombez.timurtokaev@gmail.com>
2024-03-16 12:24:47 +0300

    Register types in cms

--- apps/cms/schemaTypes/index.ts
+++ apps/cms/schemaTypes/index.ts
@@ -1,9 +1,13 @@
+import BlockImage from "./BlockImage"
+import NewsTag from "./NewsTag"
 import employee from "./employee"
 import frequentlyAskedQuestions from "./frequentlyAskedQuestions"
 import kvantum from "./kvantum"
 import news from "./news"
 
 export const schemaTypes = [
+    NewsTag,
+    BlockImage,
     employee,
     frequentlyAskedQuestions,
     kvantum,
