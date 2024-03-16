b551d8fad4705639d5deb10b335c3652c3960416
Timur <toombez.timurtokaev@gmail.com>
2024-03-16 12:25:31 +0300

    Add TheNewsSection

---
+++ apps/web/components/sections/TheNewsSection.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+const { data } = useSanityQuery<{
+    slug: string
+    previewImage: string
+    content: any[]
+    tags: string[]
+    heading: string
+    _id: string
+    publishDate: string
+}[]>(groq`
+    *[_type == 'news'] | order(publishDate desc)[0...6] {
+        'slug': slug.current,
+        'previewImage': previewImage.asset->url,
+        content[]{
+            ...,
+            _type == "imageBlock" => {
+                ...,
+                'image': image.asset->url,
+            }
+        },
+        'tags': tags[]->{ name }.name,
+        heading,
+        _id,
+        publishDate,
+    }
+`)
+</script>
+
+<template>
+    <KContainer>
+        <h1 :class="$style.heading">
+            Новости
+        </h1>
+
+        <ul :class="$style.list">
+            <li v-for="news in data">
+                <NuxtLink :to="`/news/${news.slug}`">
+                    <NewsCard
+                        :news="{
+                            previewImage: news.previewImage,
+                            publishDate: new Date(news.publishDate),
+                            tags: news.tags,
+                            title: news.heading,
+                        }"
+                    />
+                </NuxtLink>
+            </li>
+            <KLinkButton
+                to="/news"
+                variant="primary"
+                style="grid-column: 3;"
+            >
+                Все новости
+            </KLinkButton>
+        </ul>
+    </KContainer>
+</template>
+
+<style module>
+.heading {
+    font-size: 2.5rem;
+    line-height: 1.3;
+    font-family: 'BankGothic';
+    text-align: center;
+    margin-bottom: 2rem;
+}
+.list {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    /* grid-auto-rows: 1fr; */
+    gap: 0.25rem;
+}
+.list-item {
+}
+</style>
--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -3,6 +3,7 @@
         <TheHero />
         <TheInfo :class="$style.info" />
         <TheKvantumsSection />
+        <TheNewsSection :class="$style.news" />
         <TheFAQSection :class="$style.faq" />
         <TheMapSection :class="$style.map" />
     </div>
@@ -11,6 +12,9 @@
 <style module>
 .info {
     margin-bottom: 4rem;
+}
+.news {
+    margin-top: 4rem;
 }
 .faq {
     margin-top: 4rem;
