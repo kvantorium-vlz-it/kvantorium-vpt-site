c5bc90b3f8d2b1226bbd2a5929802c6c74a0c34c
Timur <toombez.timurtokaev@gmail.com>
2024-03-18 15:26:22 +0300

    Use schemas

--- apps/cms/schemaTypes/index.ts
+++ apps/cms/schemaTypes/index.ts
@@ -1,13 +1,13 @@
-import BlockImage from "./BlockImage"
-import NewsTag from "./NewsTag"
+import imageBlock from "./imageBlock"
+import newsTag from "./newsTag"
 import employee from "./employee"
 import frequentlyAskedQuestions from "./frequentlyAskedQuestions"
 import kvantum from "./kvantum"
 import news from "./news"
 
 export const schemaTypes = [
-    NewsTag,
-    BlockImage,
+    imageBlock,
+    newsTag,
     employee,
     frequentlyAskedQuestions,
     kvantum,
