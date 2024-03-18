ae9a66efc74334e2eb02537ff37eb0c4b3a3f749
Timur <toombez.timurtokaev@gmail.com>
2024-03-18 18:37:24 +0300

    Add curricula to kvantum

--- apps/web/pages/kvantum/[slug].vue
+++ apps/web/pages/kvantum/[slug].vue
@@ -8,6 +8,29 @@ const { data: kvantum } = useSanityQuery<{
     minAge: number
     icon: string
     slug: string
+    curriculas: {
+        hoursPerYear: {
+            firstHalf: number
+            secondHalf: number
+        }
+        ageFrom: number
+        ageTo: number
+        description: any[]
+        schedule: {
+            count: number
+            hours: number
+        }
+        studentsCount: number
+        name: string
+        interview: boolean
+        level: 'Вводный' | 'Углубленный' | 'Проектный'
+        teacher: {
+            name: string
+            surname: string
+            patronymic: string
+            _id: string
+        }
+    }[]
 }>(groq`
     *[_type == 'kvantum' && slug.current == $slug][0] {
         _id,
@@ -16,10 +39,35 @@ const { data: kvantum } = useSanityQuery<{
         minAge,
         'icon': icon.asset->url,
         'slug': slug.current,
+
+        'curriculas': *[_type == 'curricula' && references(^._id)] {
+            hoursPerYear,
+            ageFrom,
+            ageTo,
+            'description': description[]{
+                ...,
+                _type == "imageBlock" => {
+                    ...,
+                    'image': image.asset->url,
+                }
+            },
+            schedule,
+            studentsCount,
+            level,
+            teacher-> {
+                _id,
+                name,
+                surname,
+                patronymic,
+            },
+            name,
+            interview,
+        }
     }
 `, {
     slug,
 })
+console.log(kvantum.value?.curriculas);
 </script>
 
 <template>
@@ -40,6 +88,45 @@ const { data: kvantum } = useSanityQuery<{
                     {{ topic }}
                 </li>
             </ol>
+
+            <h2>
+                Программы
+            </h2>
+
+            <div v-for="curricula in kvantum.curriculas">
+
+                <h3>
+                    {{ curricula.name }}
+                </h3>
+
+                <p>
+                    Возраст: {{ curricula.ageFrom }}-{{ curricula.ageTo }} лет
+                </p>
+
+                <p>
+                    Уровень: {{ curricula.level }}
+                </p>
+
+                <p>
+                    Собеседование {{ curricula.interview ? 'да' : 'нет' }}
+                </p>
+
+                <p>
+                    Количество часов в год: {{ curricula.hoursPerYear.firstHalf }}/{{ curricula.hoursPerYear.secondHalf }}
+                </p>
+
+                <p>
+                    Режим занятий: {{ curricula.schedule.count }} раза в неделю по {{ curricula.schedule.hours }} академических часа
+                </p>
+
+                <p>
+                    Количество мест – 1 группа - {{ curricula.studentsCount }} чел.
+                </p>
+
+                <div>
+                    <NewsContentBlock :blocks="curricula.description" />
+                </div>
+            </div>
         </template>
 
         <h1 v-else>
