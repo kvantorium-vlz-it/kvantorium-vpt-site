eaee446b063e6ec01842901be0a91217e4beb62a
Timur <toombez.timurtokaev@gmail.com>
2024-03-28 15:49:26 +0300

    Add media

---
+++ apps/web/pages/media.vue
@@ -0,0 +1,23 @@
+<template>
+    <KContainer :class="$style.container">
+        <h1 :class="$style.heading">
+            Медиа
+        </h1>
+
+        <h2 :class="$style.heading">
+            Галерея
+        </h2>
+    </KContainer>
+</template>
+
+<style module>
+.container {
+    margin-top: 5rem;
+}
+.heading {
+    text-align: center;
+    font-size: 2rem;
+    font-family: 'BankGothic';
+    margin-bottom: 4rem;
+}
+</style>
