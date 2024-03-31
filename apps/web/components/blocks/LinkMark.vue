4bb9ec0f13d627b0261152ac61ff3ffed9f60a56
Timur <toombez.timurtokaev@gmail.com>
2024-03-31 14:40:05 +0300

    Add LinkMark

---
+++ apps/web/components/blocks/LinkMark.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+interface Props {
+    href: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <KLink
+        external
+        :to="href"
+        variant="dark"
+        :class="$style.link"
+        target="_blank"
+    >
+        <slot></slot><Icon name="ph:arrow-up-right" />
+    </KLink>
+</template>
+
+<style module>
+.link {
+    color: var(--c-site-primary-lighter-2);
+}
+.link:visited {
+    color: var(--c-site-primary);
+}
+</style>
