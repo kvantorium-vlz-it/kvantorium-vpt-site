1a142506f945f5c797f63f34a44a8cc4bbf5c281
Timur <toombez.timurtokaev@gmail.com>
2024-03-31 18:55:46 +0300

    Use BlockContent in news

--- apps/web/pages/news/[slug].vue
+++ apps/web/pages/news/[slug].vue
@@ -46,54 +46,54 @@ const formattedDate = computed(() => {
 
 <template>
     <KContainer :class="$style.page" v-if="data">
-        <div :class="$style.news">
-            <img
-                :src="data?.previewImage"
-                alt=""
-                :class="$style.image"
-            >
+        <KSection>
+            <div :class="$style.news">
+                <img
+                    :src="data?.previewImage"
+                    alt=""
+                    :class="$style.image"
+                >
 
-            <ul :class="$style.tags">
-                <li v-for="tag in data.tags">
-                    <KBadge :class="$style.tag">
-                        #{{ tag }}
-                    </KBadge>
-                </li>
-            </ul>
+                <ul :class="$style.tags">
+                    <li v-for="tag in data.tags">
+                        <KBadge :class="$style.tag">
+                            #{{ tag }}
+                        </KBadge>
+                    </li>
+                </ul>
 
-            <div :class="$style.info">
-                <h1 :class="$style.heading">
-                    {{ data?.heading }}
-                </h1>
+                <div :class="$style.info">
+                    <h1 :class="$style.heading">
+                        {{ data?.heading }}
+                    </h1>
 
-                <div>
-                    {{ formattedDate }}
+                    <div>
+                        {{ formattedDate }}
+                    </div>
+                </div>
+
+
+                <div
+                    :class="$style.content"
+                >
+
+                    <hr style="margin-bottom: 1rem; background-color: var(--c-site-background-darker-2);">
+
+                    <BlockContent :blocks="data.content" />
+
+                    <div>
+                        <KSwiper :class="$style.swiper" :visible-slides-count="2" :items="data.images" >
+                            <template #slide="{ item }">
+                                <img
+                                    :src="item.url"
+                                    :class="$style['swiper-image']"
+                                />
+                            </template>
+                        </KSwiper>
+                    </div>
                 </div>
             </div>
-
-
-            <div
-                :class="$style.content"
-            >
-
-                <hr style="margin-bottom: 1rem; background-color: var(--c-site-background-darker-2);">
-
-                <NewsContentBlock
-                    :blocks="data?.content"
-                />
-
-                <div>
-                    <KSwiper :class="$style.swiper" :visible-slides-count="2" :items="data.images" >
-                        <template #slide="{ item }">
-                            <img
-                                :src="item.url"
-                                :class="$style['swiper-image']"
-                            />
-                        </template>
-                    </KSwiper>
-                </div>
-            </div>
-        </div>
+        </KSection>
     </KContainer>
 </template>
 
@@ -140,39 +140,8 @@ const formattedDate = computed(() => {
 }
 .content {
     margin-inline: auto;
-    font-family: 'Circe';
     margin-top: 0.5rem;
-    font-size: 1.25rem;
     max-width: 50rem;
-}
-.content p {
-    text-indent: 3ch;
-    margin-block: 0.25em;
-    line-height: 1.6;
-}
-.content h1,
-.content h2,
-.content h3,
-.content h4,
-.content h5,
-.content h6 {
-    text-indent: 1ch;
-    font-family: 'BankGothic';
-    margin-top: 0.75em;
-    margin-bottom: 0.75em;
-}
-.content ol,
-.content ul {
-    list-style-position: inside;
-    padding-left: 3ch;
-}
-.content ol,
-.content ul {
-    margin-top: 0.5em;
-    margin-bottom: 0.5em;
-}
-.content li::marker {
-    margin-right: 0;
 }
 .image {
     display: block;
