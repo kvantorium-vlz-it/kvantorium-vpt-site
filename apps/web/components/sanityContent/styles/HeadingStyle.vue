01355a59622cef90d72a2874c704e7237d061eb7
Timur <toombez.timurtokaev@gmail.com>
2024-03-31 15:52:59 +0300

    Add HeadingStyles

---
+++ apps/web/components/sanityContent/styles/HeadingStyle.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+type Style = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+
+interface Props {
+    style: Style
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <KTypography
+        font-family="BankGothic"
+        :font-weight="700"
+        :font-size="style"
+        #="{ classes }"
+    >
+        <component
+            :is="style"
+            :class="[classes, $style.heading]"
+            :="$attrs, $props"
+        >
+
+            <slot></slot>
+        </component>
+    </KTypography>
+</template>
+
+<style module>
+.heading {
+    margin-block: 1em;
+}
+</style>
