6c3bf8acd418397b009fc961a8808c01b3cbb511
Timur <toombez.timurtokaev@gmail.com>
2024-03-31 14:59:48 +0300

    Add Em and Strong marks

---
+++ apps/web/components/sanityContent/marks/EmMark.vue
@@ -0,0 +1,11 @@
+<template>
+    <em :class="$style.em">
+        <slot></slot>
+    </em>
+</template>
+
+<style module>
+.em {
+    font-style: italic;
+}
+</style>
---
+++ apps/web/components/sanityContent/marks/StrongMark.vue
@@ -0,0 +1,11 @@
+<template>
+    <strong :class="$style.strong">
+        <slot></slot>
+    </strong>
+</template>
+
+<style module>
+.strong {
+    font-weight: 700;
+}
+</style>
