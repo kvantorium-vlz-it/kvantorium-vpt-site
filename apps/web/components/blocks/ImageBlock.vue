011429dbed76d57c0dc0bc452f7281b6252a4897
Timur <toombez.timurtokaev@gmail.com>
2024-03-31 14:42:59 +0300

    Add ImageBlock

---
+++ apps/web/components/blocks/ImageBlock.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+    image: string
+    alt: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <img
+        :src="image"
+        :alt="alt"
+        :class="$style.image"
+    >
+</template>
+
+<style module>
+.image {
+    display: block;
+    width: 100%;
+    margin-block: 1em;
+    border-radius: 1rem;
+}
+</style>
