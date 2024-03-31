e8c033a86ca3be6020a6b0f365229edb50d4d9ae
Timur <toombez.timurtokaev@gmail.com>
2024-03-31 14:49:36 +0300

    Add StrikeThroughMark

---
+++ apps/web/components/sanityContent/BlockContent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ImageBlock, LinkMark, SanityContent, SpanBlock } from '#components'
+
+type SanityContentProps = InstanceType<typeof SanityContent>['$props']
+type SanityContentSerializer = SanityContentProps['serializers']
+
+defineProps<{
+    blocks: SanityContentProps['blocks']
+}>()
+
+const serializer: SanityContentSerializer = {
+    types: {
+        imageBlock: ImageBlock,
+        span: SpanBlock
+    },
+    marks: {
+        link: LinkMark,
+    }
+}
+</script>
+
+<template>
+    <SanityContent :blocks="blocks" :serializers="serializer" />
+</template>
