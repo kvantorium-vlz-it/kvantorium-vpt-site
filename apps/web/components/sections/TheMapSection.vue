c598f2e5d1a981a0bafdc9c7d7ae9228f2935db1
Timur <toombez.timurtokaev@gmail.com>
2024-04-05 22:36:37 +0300

    Add Interactivity to map

--- apps/web/components/sections/TheMapSection.vue
+++ apps/web/components/sections/TheMapSection.vue
@@ -1,32 +1,116 @@
 <script setup lang="ts">
-import { YandexMap, YandexMapDefaultSchemeLayer, YandexMapZoomControl, YandexMapScaleControl, YandexMapGeolocationControl, YandexMapControls, YandexMapDefaultFeaturesLayer } from 'vue-yandex-maps';
+import {
+    YandexMap,
+    YandexMapDefaultSchemeLayer,
+    YandexMapDefaultMarker,
+    YandexMapDefaultFeaturesLayer,
+    YandexMapZoomControl,
+    YandexMapGeolocationControl,
+    YandexMapControls,
+    YandexMapControlButton,
+    type YandexMapSettings,
+} from 'vue-yandex-maps';
+
+import type {
+    YMapCameraRequest,
+} from '@yandex/ymaps3-types/imperative/YMap'
+
+const map = ref<HTMLDivElement>()
+onClickOutside(map, () => {
+    isFocused.value = false
+})
+
+const [isFocused] = useToggle(false)
+
+type BehaviourType = YandexMapSettings['behaviors']
+
+const behaviors = computed<BehaviourType>(() => {
+    const shared: BehaviourType = ['drag', 'pinchZoom', 'mouseTilt', 'mouseRotate']
+
+    return isFocused.value ? [...shared, 'scrollZoom'] : shared
+})
+
+const camera = ref<YMapCameraRequest>({
+    tilt: (45 * Math.PI) / 180,
+    duration: 2500,
+})
+
+const location = ref({
+    center: [44.772160, 48.786934],
+    zoom: 16
+})
 </script>
 
 <template>
     <KContainer>
-        <KSection heading="мы на карте">
-            <div :class="$style['map-wrapper']">
+        <KSection heading="Мы на карте">
+            <div :class="[$style['map-wrapper'], { [$style.focused]: isFocused }]">
                 <ClientOnly>
                     <YandexMap
+                        ref="map"
+                        @mousedown="() => { isFocused = true }"
+                        @mouseleave="() => { isFocused = false }"
                         :settings="{
                             location: {
-                                center: [44.772160, 48.786934],
-                                zoom: 18,
-                                easing: { p1: { x: 1, y: 4 }, p2: { x: 2, y: 4 } }
+                                ...location,
+                                duration: camera.duration,
                             },
-                            camera: {
-                                tilt: Math.PI * 45 / 180,
-                            }
+                            camera: camera,
+                            behaviors: behaviors,
+                            showScaleInCopyrights: true,
                         }"
                         width="100%"
                         height="22rem"
                         :class="$style.map"
+                        real-settings-location
                     >
                         <YandexMapDefaultSchemeLayer />
-                        <YandexMapControls :settings="{ position: 'left' }">
+                        <YandexMapDefaultFeaturesLayer />
+
+                        <!-- <YandexMapControls :settings="{ position: 'top' }">
+                            <YandexMapControlButton
+                                style="align-items: center; display: grid; grid-template-columns: auto 1fr; gap: 0.5rem; max-width: 20rem; white-space: wrap;"
+                                :settings="{
+                                    onClick: () => {
+                                        location.center = [44.772160, 48.786934]
+                                    }
+                                }"
+                            >
+                                <Icon style="font-size: 2rem;" name="ph:map-pin" />
+
+                                Детский технопарк "Кванториум "Волжский Политех"
+                            </YandexMapControlButton>
+                            <YandexMapControlButton
+                                style="align-items: center; display: grid; grid-template-columns: auto 1fr; gap: 0.5rem; max-width: 20rem; white-space: wrap;"
+                                :settings="{
+                                    onClick: () => {
+                                        location.center = [44.729346, 48.807840]
+                                    }
+                                }"
+                            >
+                                <Icon style="font-size: 2rem;" name="ph:map-pin" />
+
+                                Волжский политехнический техникум
+                            </YandexMapControlButton>
+                        </YandexMapControls> -->
+
+                        <YandexMapControls :settings="{ position: 'right' }">
                             <YandexMapZoomControl />
                             <YandexMapGeolocationControl />
                         </YandexMapControls>
+
+                        <YandexMapDefaultMarker
+                            :settings="{
+                                coordinates: [44.772160, 48.786934],
+                                color: '#213A8F',
+                            }"
+                        />
+                        <!-- <YandexMapDefaultMarker
+                            :settings="{
+                                coordinates: [44.729346, 48.807840],
+                                color: '#3E5062',
+                            }"
+                        /> -->
                     </YandexMap>
                 </ClientOnly>
             </div>
@@ -40,13 +124,21 @@ import { YandexMap, YandexMapDefaultSchemeLayer, YandexMapZoomControl, YandexMap
 }
 .map-wrapper {
     height: 22rem;
-    margin-top: 2rem;
+}
+.map-wrapper:hover > .map {
+    box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
+}
+.focused {
+    outline: 4px dashed var(--c-site-background-darker-2);
+    outline-offset: 4px;
+    border-radius: 0.75rem;
 }
 .map {
     height: 100%;
     border-radius: 0.75rem;
     border: 1px solid var(--c-site-background-darker-2);
     overflow: hidden;
+    transition: 0.2s ease-out;
 }
 .heading {
     font-size: 2.5rem;
