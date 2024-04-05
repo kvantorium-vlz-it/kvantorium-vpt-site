1a324200bb3ce3c2229e01613aec85a556849ff9
Timur <toombez.timurtokaev@gmail.com>
2024-04-05 22:41:05 +0300

    Add contacts to map

--- apps/web/components/sections/TheMapSection.vue
+++ apps/web/components/sections/TheMapSection.vue
@@ -44,76 +44,105 @@ const location = ref({
 <template>
     <KContainer>
         <KSection heading="Мы на карте">
-            <div :class="[$style['map-wrapper'], { [$style.focused]: isFocused }]">
-                <ClientOnly>
-                    <YandexMap
-                        ref="map"
-                        @mousedown="() => { isFocused = true }"
-                        @mouseleave="() => { isFocused = false }"
-                        :settings="{
-                            location: {
-                                ...location,
-                                duration: camera.duration,
+            <KGrid :columns="4">
+                <KGridCell>
+                    <KNavigation
+                        :items="[
+                            {
+                                label: 'kvantoriumvlz@volpt.com',
+                                iconName: 'mingcute:mail-fill',
+                                to: 'mailto:kvantoriumvlz@volpt.com',
                             },
-                            camera: camera,
-                            behaviors: behaviors,
-                            showScaleInCopyrights: true,
-                        }"
-                        width="100%"
-                        height="22rem"
-                        :class="$style.map"
-                        real-settings-location
-                    >
-                        <YandexMapDefaultSchemeLayer />
-                        <YandexMapDefaultFeaturesLayer />
+                            {
+                                label: '8(902)383-50-24',
+                                iconName: 'ic:baseline-phone',
+                                to: 'tel:89023835024',
+                            },
+                            {
+                                label: 'Волжский ул. Машиностроителей, 15, 404121',
+                                iconName: 'bi:geo-alt-fill',
+                                to: 'geo:48.786934,44.772160',
+                            },
+                        ]"
+                        variant="dark"
+                        direction="vertical"
+                        heading="Кванториум Волжский Политех"
+                    />
+                </KGridCell>
 
-                        <!-- <YandexMapControls :settings="{ position: 'top' }">
-                            <YandexMapControlButton
-                                style="align-items: center; display: grid; grid-template-columns: auto 1fr; gap: 0.5rem; max-width: 20rem; white-space: wrap;"
+                <KGridCell :start-column="2" :end-column="5">
+                    <div :class="[$style['map-wrapper'], { [$style.focused]: isFocused }]">
+                        <ClientOnly>
+                            <YandexMap
+                                ref="map"
+                                @mousedown="() => { isFocused = true }"
+                                @mouseleave="() => { isFocused = false }"
                                 :settings="{
-                                    onClick: () => {
-                                        location.center = [44.772160, 48.786934]
-                                    }
+                                    location: {
+                                        ...location,
+                                        duration: camera.duration,
+                                    },
+                                    camera: camera,
+                                    behaviors: behaviors,
+                                    showScaleInCopyrights: true,
                                 }"
+                                width="100%"
+                                height="22rem"
+                                :class="$style.map"
+                                real-settings-location
                             >
-                                <Icon style="font-size: 2rem;" name="ph:map-pin" />
+                                <YandexMapDefaultSchemeLayer />
+                                <YandexMapDefaultFeaturesLayer />
 
-                                Детский технопарк "Кванториум "Волжский Политех"
-                            </YandexMapControlButton>
-                            <YandexMapControlButton
-                                style="align-items: center; display: grid; grid-template-columns: auto 1fr; gap: 0.5rem; max-width: 20rem; white-space: wrap;"
-                                :settings="{
-                                    onClick: () => {
-                                        location.center = [44.729346, 48.807840]
-                                    }
-                                }"
-                            >
-                                <Icon style="font-size: 2rem;" name="ph:map-pin" />
+                                <!-- <YandexMapControls :settings="{ position: 'top' }">
+                                    <YandexMapControlButton
+                                        style="align-items: center; display: grid; grid-template-columns: auto 1fr; gap: 0.5rem; max-width: 20rem; white-space: wrap;"
+                                        :settings="{
+                                            onClick: () => {
+                                                location.center = [44.772160, 48.786934]
+                                            }
+                                        }"
+                                    >
+                                        <Icon style="font-size: 2rem;" name="ph:map-pin" />
 
-                                Волжский политехнический техникум
-                            </YandexMapControlButton>
-                        </YandexMapControls> -->
+                                        Детский технопарк "Кванториум "Волжский Политех"
+                                    </YandexMapControlButton>
+                                    <YandexMapControlButton
+                                        style="align-items: center; display: grid; grid-template-columns: auto 1fr; gap: 0.5rem; max-width: 20rem; white-space: wrap;"
+                                        :settings="{
+                                            onClick: () => {
+                                                location.center = [44.729346, 48.807840]
+                                            }
+                                        }"
+                                    >
+                                        <Icon style="font-size: 2rem;" name="ph:map-pin" />
 
-                        <YandexMapControls :settings="{ position: 'right' }">
-                            <YandexMapZoomControl />
-                            <YandexMapGeolocationControl />
-                        </YandexMapControls>
+                                        Волжский политехнический техникум
+                                    </YandexMapControlButton>
+                                </YandexMapControls> -->
 
-                        <YandexMapDefaultMarker
-                            :settings="{
-                                coordinates: [44.772160, 48.786934],
-                                color: '#213A8F',
-                            }"
-                        />
-                        <!-- <YandexMapDefaultMarker
-                            :settings="{
-                                coordinates: [44.729346, 48.807840],
-                                color: '#3E5062',
-                            }"
-                        /> -->
-                    </YandexMap>
-                </ClientOnly>
-            </div>
+                                <YandexMapControls :settings="{ position: 'right' }">
+                                    <YandexMapZoomControl />
+                                    <YandexMapGeolocationControl />
+                                </YandexMapControls>
+
+                                <YandexMapDefaultMarker
+                                    :settings="{
+                                        coordinates: [44.772160, 48.786934],
+                                        color: '#213A8F',
+                                    }"
+                                />
+                                <!-- <YandexMapDefaultMarker
+                                    :settings="{
+                                        coordinates: [44.729346, 48.807840],
+                                        color: '#3E5062',
+                                    }"
+                                /> -->
+                            </YandexMap>
+                        </ClientOnly>
+                    </div>
+                </KGridCell>
+            </KGrid>
         </KSection>
     </KContainer>
 </template>
