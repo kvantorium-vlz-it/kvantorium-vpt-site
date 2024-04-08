0bdc45429ff4455ad5d0b7bf69f1b8f663ad65c9
Timur <toombez.timurtokaev@gmail.com>
2024-04-08 21:55:55 +0300

    Add new kvantum schema

---
+++ apps/cms/src/schemas/documents/kvantum.ts
@@ -0,0 +1,42 @@
+import { defineField, defineType } from "sanity"
+import { DocumentIcon } from '@sanity/icons'
+import imageAsset from "../objects/imageAsset"
+import contentBlocks from "../objects/contentBlock"
+
+export default defineType({
+    name: 'kvantum',
+    type: 'document',
+    title: 'Квантумы',
+    icon: DocumentIcon,
+    fields: [
+        defineField({
+            name: 'name',
+            type: 'string',
+            title: 'Название квантума',
+            validation: (rule) => rule
+                .required()
+                .error("Поле обязательно для заполнения."),
+        }),
+
+        defineField({
+            name: 'slug',
+            type: 'slug',
+            title: 'человекочитаемая ссылка',
+            options: {
+                source: 'name',
+            },
+        }),
+
+        defineField({
+            name: 'icon',
+            type: imageAsset.name,
+            title: 'Иконка квантума',
+        }),
+
+        defineField({
+            name: 'description',
+            type: contentBlocks.name,
+            title: 'Описание квантума',
+        }),
+    ],
+})
