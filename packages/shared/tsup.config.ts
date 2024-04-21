430be38fc2edb511f3f74eedf5dad4ceefee2dc3
Timur <toombez.timurtokaev@gmail.com>
2024-04-21 22:17:33 +0300

    Init package with tsup

---
+++ packages/shared/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig((options) => {
+    return {
+        entry: ['src/index.ts'],
+        outDir: 'dist',
+        clean: !options.watch,
+        dts: true,
+        minify: !options.watch,
+        format: ['cjs', 'esm'],
+        sourcemap: !!options.watch,
+    }
+})
