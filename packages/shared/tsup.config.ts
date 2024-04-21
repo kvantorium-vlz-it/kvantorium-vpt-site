6f25db765f4d73057f83fb664eef31205db6cc25
Timur <toombez.timurtokaev@gmail.com>
2024-04-21 23:14:34 +0300

    Add multiple entry

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig((options) => {
     return {
-        entry: ['src/index.ts'],
+        entry: [
+            'src/index.ts',
+            'src/constants/index.ts',
+            'src/enums.ts',
+        ],
         outDir: 'dist',
         clean: !options.watch,
         dts: true,
         minify: !options.watch,
         format: ['cjs', 'esm'],
         sourcemap: !!options.watch,
+        treeshake: !options.watch,
     }
 })
