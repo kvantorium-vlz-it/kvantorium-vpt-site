77ad48634826e40319fe0da6550c6727a1313ec7
Timur <toombez.timurtokaev@gmail.com>
2024-04-23 01:12:29 +0300

    Add useSwiper example

---
+++ packages/ui/playground/pages/swiper.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+const slides = ref(10)
+const slidesPerView = ref(2)
+
+const {
+    currentView,
+    viewsCount,
+    viewSlides,
+    slideToNextView,
+    slideToPreviousView,
+    slideToFirstView,
+    slideToLastView,
+    isFirstView,
+    isLastView,
+} = useSwiper({
+    slidesCount: slides,
+    slidesPerView: slidesPerView
+})
+</script>
+
+<template>
+    <div>
+        <hr>
+        Views: {{ viewsCount }}
+        <br>
+        Current view: {{ currentView }}
+        <hr>
+
+        <div :class="$style.grid">
+            <div
+                :class="$style.cell"
+                v-for="i in slides"
+                :style="{
+                    background: viewSlides.includes(i - 1) ? 'red' : 'white'
+                }"
+            >
+                {{ i - 1 }}
+            </div>
+        </div>
+
+        <hr>
+
+        <button @click="slideToPreviousView" :disabled="isFirstView">
+            prev
+        </button>
+        <button @click="slideToNextView" :disabled="isLastView">
+            next
+        </button>
+
+        <hr>
+
+        <button @click="slides += 1">
+            add slide
+        </button>
+        <button @click="slides -= 1">
+            remove slide
+        </button>
+        <br>
+        <button @click="slidesPerView += 1">
+            add slide to view
+        </button>
+        <button @click="slidesPerView -= 1">
+            remove slide from view
+        </button>
+
+        <hr>
+
+        <button @click="slideToFirstView" >
+            slide to first
+        </button>
+        <button @click="slideToLastView" >
+            slide to last
+        </button>
+
+        <hr>
+        Visible slides: {{ viewSlides }}
+    </div>
+</template>
+
+<style module>
+.grid {
+    display: grid;
+    grid-template-columns: repeat(v-bind(slides), 1fr);
+}
+.cell {
+    border: 1px solid black;
+    aspect-ratio: 1;
+}
+</style>
