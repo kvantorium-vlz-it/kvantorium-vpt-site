020b1d1d5c925ca982e616676eade943be9c46ca
Timur <toombez.timurtokaev@gmail.com>
2024-04-22 18:57:13 +0300

    Use new file in apps

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -24,12 +24,18 @@ export default defineNuxtConfig({
         'vue-final-modal/style.css',
         '~/assets/css/index.css',
     ],
+    typescript: {
+        includeWorkspace: true,
+        tsConfig: {
+            include: ["@kvantorium-vpt-site/tsconfig/enviroments.d.ts"]
+        }
+    },
     yandexMaps: {
         apikey: process.env.YANDEX_MAPS_API_KEY,
     },
     sanity: {
-        projectId: process.env.SANITY_PROJECT_ID,
+        projectId: process.env.NUXT_SANITY_PROJECT_ID,
         dataset: process.env.SANITY_DATASET,
-        apiVersion: MetaConstants.SANITY_API_VERSION,
-    }
+        apiVersion: process.env.SANITY_API_VERSION,
+    },
 })
