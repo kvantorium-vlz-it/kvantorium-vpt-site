244c7c0e1804a4ffae6b902c4a540723a8f4f198
Timur <toombez.timurtokaev@gmail.com>
2024-04-23 17:54:19 +0300

    Change TheKvantumsSection

--- apps/web/components/KvantumsSwiper.vue
+++ apps/web/components/KvantumsSwiper.vue
@@ -24,5 +24,51 @@ defineProps<Props>()
                 :kvantum="kvantum"
             />
         </KSwiperSlide>
+
+        <template
+            #navigation="{
+                isFirstView,
+                isLastView,
+                slideToNextView,
+                slideToPreviousView,
+                viewsCount,
+                currentView,
+            }"
+        >
+            <KProgress
+                :max="viewsCount"
+                :value="currentView"
+                :class="$style.progress"
+            />
+
+            <div :class="$style.buttons">
+                <KCircleButton
+                    @click="slideToPreviousView"
+                    :is-disabled="isFirstView"
+                >
+                    <Icon name="ph:arrow-left" />
+                </KCircleButton>
+                <KCircleButton
+                    @click="slideToNextView"
+                    :is-disabled="isLastView"
+                >
+                    <Icon name="ph:arrow-right" />
+                </KCircleButton>
+            </div>
+        </template>
     </KSwiper>
 </template>
+
+<style module>
+.buttons {
+    display: flex;
+    justify-content: center;
+    gap: 0.5rem;
+    margin-top: 0.5rem;
+}
+.progress {
+    max-width: calc(100% / var(--slides-per-view));
+    margin-inline: auto;
+    margin-top: 0.5rem;
+}
+</style>
--- apps/web/components/sections/TheKvantumsSection.vue
+++ apps/web/components/sections/TheKvantumsSection.vue
@@ -25,13 +25,17 @@ const { data } = useSanityQuery<{
 <template>
     <KContainer :class="$style.container">
         <KSection heading="Квантумы">
-            <div :class="$style.kvantums">
-                <TheKvantumsSectionInfoCard :class="$style.info" />
+            <KGrid :columns="5">
+                <KGridCell>
+                    <TheKvantumsSectionInfoCard />
+                </KGridCell>
 
-                <KvantumsSwiper
-                    :kvantums="data || []"
-                />
-            </div>
+                <KGridCell :width="4">
+                    <KvantumsSwiper
+                        :kvantums="data || []"
+                    />
+                </KGridCell>
+            </KGrid>
         </KSection>
     </KContainer>
 </template>
@@ -60,6 +64,12 @@ const { data } = useSanityQuery<{
 .swiper {
     --slides: 1;
     width: calc(1 * 50% - 0.5rem + 0.5rem);
+}
+.paragraph {
+    max-width: 40rem;
+    margin-block-end: 1rem;
+    margin-inline: auto;
+    text-align: center
 }
 @media screen and (min-width: 768px) {
     .info {
