daaf343016af3cd4aecc3d6964304b4e5003d599
Timur <toombez.timurtokaev@gmail.com>
2024-04-25 15:24:20 +0300

    Add Card page in playground

---
+++ packages/ui/playground/pages/card.vue
@@ -0,0 +1,60 @@
+<template>
+    <KCard>
+        <KCardInset>
+            <img
+                src="https://us.123rf.com/450wm/zimtastic/zimtastic2304/zimtastic230400237/203273296-lonely-tree-at-sunset-with-beautiful-clouds-in-the-sky-negative-space.jpg?ver=6"
+                alt=""
+                style="width: 100%; display: block;"
+            >
+        </KCardInset>
+
+        <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et repudiandae soluta aut! Nisi voluptas maxime cum est quas blanditiis et sit quos culpa tempora assumenda eos rem corrupti, quo recusandae!
+        </div>
+
+
+        <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et repudiandae soluta aut! Nisi voluptas maxime cum est quas blanditiis et sit quos culpa tempora assumenda eos rem corrupti, quo recusandae!
+        </div>
+
+        <KCardInset>
+            <img
+                src="https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=600&h=400"
+                alt=""
+                style="width: 100%; display: block;"
+            >
+        </KCardInset>
+
+        <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, possimus!
+        </div>
+
+        <KCardInset>
+            <img
+                src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
+                alt=""
+                style="width: 100%; display: block;"
+            >
+        </KCardInset>
+    </KCard>
+
+    <KCard>
+        <KCardHeader>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore optio quibusdam itaque ipsa consequuntur harum molestiae veniam delectus. Beatae laudantium, nam optio quae necessitatibus ducimus numquam ab nostrum sed possimus?
+        </KCardHeader>
+
+        <KCardHeader>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore optio quibusdam itaque ipsa consequuntur harum molestiae veniam delectus. Beatae laudantium, nam optio quae necessitatibus ducimus numquam ab nostrum sed possimus?
+        </KCardHeader>
+
+        <KCardBody style="margin-top: 2rem; margin-bottom: 0.5rem;">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, reprehenderit.
+        </KCardBody>
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, vel!
+
+        <KCardHeader>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore optio quibusdam itaque ipsa consequuntur harum molestiae veniam delectus. Beatae laudantium, nam optio quae necessitatibus ducimus numquam ab nostrum sed possimus?
+        </KCardHeader>
+    </KCard>
+</template>
