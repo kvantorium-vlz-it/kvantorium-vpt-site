858384131fa1a710edd9603e563d2d6257c54e03
Timur <toombez.timurtokaev@gmail.com>
2024-04-27 01:07:39 +0300

    Add BaseButton

---
+++ packages/ui/src/runtime/components/base/BaseButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineNuxtLink } from '#imports';
+import type { RouterLinkProps } from '#vue-router'
+
+interface Props extends Omit<RouterLinkProps, 'to'> {
+    to?: RouterLinkProps['to']
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <component
+        :is="!to ? 'button' : defineNuxtLink($props)"
+        :="$props, $attrs"
+    >
+        <slot></slot>
+    </component>
+</template>
