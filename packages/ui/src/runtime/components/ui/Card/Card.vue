29c8943403110370a71c01402a206a06f8ab34c8
Timur <toombez.timurtokaev@gmail.com>
2024-04-27 00:29:21 +0300

    Change card border-radius

--- packages/ui/src/runtime/components/ui/Card/Card.vue
+++ packages/ui/src/runtime/components/ui/Card/Card.vue
@@ -8,7 +8,7 @@
 .card {
     --padding: 1rem;
 
-    border-radius: 0.75rem;
+    border-radius: 1rem;
     border: 1px solid var(--c-site-background-darker-2);
     background-color: var(--c-site-background);
 }
