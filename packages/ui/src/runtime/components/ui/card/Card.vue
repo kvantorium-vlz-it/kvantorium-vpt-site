9d01a435b100b248c8d62cd25ea788de7b752fa2
Timur <toombez.timurtokaev@gmail.com>
2024-04-29 14:48:51 +0300

    Make card is flexbox

--- packages/ui/src/runtime/components/ui/card/Card.vue
+++ packages/ui/src/runtime/components/ui/card/Card.vue
@@ -11,5 +11,8 @@
     border-radius: 1rem;
     border: 1px solid var(--c-site-background-darker-2);
     background-color: var(--c-site-background);
+
+    display: flex;
+    flex-direction: column;
 }
 </style>
