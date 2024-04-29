04797ff410b70c0e3e67daad3513818ad99e4f38
Timur <toombez.timurtokaev@gmail.com>
2024-04-29 14:49:40 +0300

    Add DocumentCard

---
+++ apps/web/components/DocumentCard.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+interface Props {
+    document: {
+        url: string
+        size: number
+        name: string
+    }
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <NuxtLink
+        download
+        target="_blank"
+        :to="document.url"
+        style="
+            text-decoration: none;
+            color: var(--c-site-text);
+        "
+        :class="$style.link"
+    >
+        <KCard>
+            <KCardSection>
+                <KTypography
+                    font-family="Circe"
+                    font-size="h3"
+                    #="{ classes }"
+                >
+                    <h3 :class="[classes, $style.name]">
+                        {{ document.name }}
+                    </h3>
+                </KTypography>
+            </KCardSection>
+            <KCardSection style="text-align: end; font-size: 1.5rem;">
+                <Icon :class="$style.icon" name="tabler:download" />
+            </KCardSection>
+        </KCard>
+    </NuxtLink>
+</template>
+
+<style module>
+.name,
+.icon {
+    color: var(--c-site-text);
+    transition: 0.2s ease-in-out;
+}
+.link:hover .name,
+.link:hover .icon {
+    color: var(--c-site-primary);
+}
+</style>
