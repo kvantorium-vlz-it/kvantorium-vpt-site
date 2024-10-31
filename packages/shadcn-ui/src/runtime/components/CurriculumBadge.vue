b9057e9401eb3bbac0083b29a59fab33a30d7f1d
Timur <toombez.timurtokaev@gmail.com>
2024-10-31 16:30:52 +0300

    Add curriculum badge

---
+++ packages/shadcn-ui/src/runtime/components/CurriculumBadge.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ArrowUpRightIcon } from 'lucide-vue-next'
+
+defineProps<{
+    curriculum: string
+}>()
+</script>
+
+<template>
+    <ShBadge variant="outline" :class="$style.badge">
+        <span>
+            {{ curriculum }}
+        </span>
+
+        <ArrowUpRightIcon
+            :size="16"
+            stroke-width="1"
+        />
+    </ShBadge>
+</template>
+
+<style module>
+.badge {
+    font-family: 'Circe';
+    font-weight: 400;
+    font-size: 1rem;
+}
+</style>
