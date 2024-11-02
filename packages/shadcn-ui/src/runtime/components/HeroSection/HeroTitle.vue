6fcc0e42726bd162d17752df997a751bd1ccb212
Timur <toombez.timurtokaev@gmail.com>
2024-11-02 11:42:08 +0300

    Change hero title line-height

--- packages/shadcn-ui/src/runtime/components/HeroSection/HeroTitle.vue
+++ packages/shadcn-ui/src/runtime/components/HeroSection/HeroTitle.vue
@@ -1,13 +1,13 @@
 
 <template>
     <h1
-        class="font-display text-center uppercase leading-4 -tracking-widest text-white"
+        class="font-display text-center uppercase -tracking-widest text-white"
     >
-        <span class="text-5xl xl:text-8xl">
+        <span class="!leading-8 text-5xl xl:text-8xl">
             Детский технопарк
         </span>
         <br>
-        <span class="text-3xl xl:text-6xl">
+        <span class="!leading-8 text-3xl xl:text-6xl">
             "Кванториум "волжский политех"
         </span>
     </h1>
