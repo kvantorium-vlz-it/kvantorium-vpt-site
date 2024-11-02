98bf46f7b22a1d904ccd2454a696a1968c62ffd0
Timur <toombez.timurtokaev@gmail.com>
2024-11-02 11:43:51 +0300

    Add hero section

---
+++ packages/shadcn-ui/src/runtime/components/HeroSection/TheHero.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="overflow-hidden">
+        <div
+            class="
+                bg-black h-full p-9 bg-no-repeat bg-cover bg-top min-h-screen
+                relative bg-hero-image
+                after:absolute after:top-0 after:left-0 after:bottom-0 after:right-0
+                after:bg-hero-noice after:mix-blend-overlay after:pointer-events-none
+            "
+        >
+
+            <HeroTitle class="mt-20 xl:mt-28" />
+
+            <HeroAddress class="mt-8" />
+
+            <div class="grid grid-cols-[minmax(0,1fr)_auto_minmax(0,1fr)] mt-16">
+                <HeroSiteNavigation />
+
+                <div class="w-10 aspect-square bg-red-500"></div>
+
+                <HeroKvantumsNavigation class="justify-self-end" />
+            </div>
+        </div>
+    </div>
+</template>
