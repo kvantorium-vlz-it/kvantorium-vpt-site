71562abbc01d6a4072e4fc3227e3cbb769db3568
Timur <toombez.timurtokaev@gmail.com>
2024-11-02 11:42:20 +0300

    Add hero site navigation

---
+++ packages/shadcn-ui/src/runtime/components/HeroSection/HeroSiteNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2 class="font-display text-white uppercase text-4xl">
+            Навигация
+        </h2>
+
+        <ShNavigationMenu orientation="vertical" class="mt-2">
+            <ShNavigationMenuList class="flex-col items-start gap-0.5">
+                <HeroSiteNavigationItem>
+                    Информация
+                </HeroSiteNavigationItem>
+
+                <HeroSiteNavigationItem>
+                    Квантумы
+                </HeroSiteNavigationItem>
+
+                <HeroSiteNavigationItem>
+                    новости
+                </HeroSiteNavigationItem>
+
+                <HeroSiteNavigationItem>
+                    часто задаваемые вопросы
+                </HeroSiteNavigationItem>
+
+                <HeroSiteNavigationItem>
+                    мы на карте
+                </HeroSiteNavigationItem>
+            </ShNavigationMenuList>
+        </ShNavigationMenu>
+    </div>
+</template>
