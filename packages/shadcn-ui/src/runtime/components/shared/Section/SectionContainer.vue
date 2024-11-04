1e6cb8c119132c4005712bf3bf180b2bad66f4b7
Timur <toombez.timurtokaev@gmail.com>
2024-11-04 18:07:51 +0300

    Add SectionContainer

---
+++ packages/shadcn-ui/src/runtime/components/shared/Section/SectionContainer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Primitive, type PrimitiveProps } from 'radix-vue'
+
+const props = defineProps<PrimitiveProps>()
+</script>
+
+<template>
+    <Primitive
+        class="container"
+        :="props"
+    >
+        <slot></slot>
+    </Primitive>
+</template>
