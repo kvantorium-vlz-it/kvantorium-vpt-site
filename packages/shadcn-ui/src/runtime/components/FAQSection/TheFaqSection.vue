d81641539b84c5eb1681f6043b928e33dfb981d7
Timur <toombez.timurtokaev@gmail.com>
2024-11-04 21:02:05 +0300

    Add faq section

---
+++ packages/shadcn-ui/src/runtime/components/FAQSection/TheFaqSection.vue
@@ -0,0 +1,27 @@
+<template>
+    <Section>
+        <SectionHeading>
+            Часто задаваемые вопросы
+        </SectionHeading>
+
+        <div class="mx-auto max-w-[600px]">
+            <FaqAccordion>
+                <FaqAccordionItem title="Это бесплатно" value="item-1">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, nisi.
+                </FaqAccordionItem>
+
+                <FaqAccordionItem title="Это бесплатно" value="item-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, nisi.
+                </FaqAccordionItem>
+
+                <FaqAccordionItem title="Это бесплатно" value="item-3">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, nisi.
+                </FaqAccordionItem>
+
+                <FaqAccordionItem title="Это бесплатно" value="item-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, nisi.
+                </FaqAccordionItem>
+            </FaqAccordion>
+        </div>
+    </Section>
+</template>
