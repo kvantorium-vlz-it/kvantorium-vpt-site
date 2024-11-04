5c2416ea4eb16c0ad0410584e2e48c03af5a3650
Timur <81884993+toombez@users.noreply.github.com>
2024-11-04 18:08:28 +0300

    Feat/section (#96)
    * Add Section
    
    * Add SectionHeading
    
    * Add SectionContainer
    
    * Use Primitive in Section

---
+++ packages/shadcn-ui/src/runtime/components/shared/Section/Section.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Primitive, type PrimitiveProps } from 'radix-vue'
+
+const props = withDefaults(defineProps<PrimitiveProps>(), {
+    as: 'section'
+})
+</script>
+
+<template>
+    <Primitive
+        class="my-24"
+        :="props"
+    >
+        <slot></slot>
+    </Primitive>
+</template>
---
+++ packages/shadcn-ui/src/runtime/components/shared/Section/SectionHeading.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Primitive, type PrimitiveProps } from 'radix-vue';
+
+const props = withDefaults(defineProps<PrimitiveProps>(), {
+    as: 'div'
+})
+</script>
+
+<template>
+    <Primitive
+        :="props"
+        class="font-display text-6xl text-center mb-12"
+    >
+        <slot></slot>
+    </Primitive>
+</template>
