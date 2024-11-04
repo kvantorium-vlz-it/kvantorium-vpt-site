7c3d156fdac32d508298314ff8b010ea17b16c10
Timur <toombez.timurtokaev@gmail.com>
2024-11-04 17:27:57 +0300

    Add Section

---
+++ packages/shadcn-ui/src/runtime/components/shared/Section/Section.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="my-24">
+        <div class="container">
+            <div
+                v-if="$slots.heading"
+                class="font-display text-6xl text-center mb-12"
+            >
+                <slot name="heading"></slot>
+            </div>
+
+            <div>
+                <slot></slot>
+            </div>
+        </div>
+    </section>
+</template>
