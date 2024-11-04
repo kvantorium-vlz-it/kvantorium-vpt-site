aa38b0b6abddc13b38e8802b4fd6547030293579
Timur <toombez.timurtokaev@gmail.com>
2024-11-04 21:01:58 +0300

    Add faq accordion item

---
+++ packages/shadcn-ui/src/runtime/components/FAQSection/FaqAccordionItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ChevronDownIcon } from 'lucide-vue-next'
+import { VisuallyHidden } from 'radix-vue';
+
+defineProps<{
+    title: string
+    value: string
+}>()
+</script>
+
+<template>
+    <ShAccordionItem :value="value">
+        <ShAccordionTrigger class="justify-end relative">
+            <span class="absolute left-1/2 -translate-x-1/2">
+                {{ title }}
+            </span>
+        </ShAccordionTrigger>
+
+        <ShAccordionContent class="text-center">
+            <slot></slot>
+        </ShAccordionContent>
+    </ShAccordionItem>
+</template>
