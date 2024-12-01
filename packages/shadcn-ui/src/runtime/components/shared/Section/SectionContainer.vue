77d5a19fd1f2642cca80d6f79fbb04f2c55b0a4d
Timur <toombez.timurtokaev@gmail.com>
2024-12-02 01:09:05 +0300

    Allow container being full-width

--- packages/shadcn-ui/src/runtime/components/shared/Section/SectionContainer.vue
+++ packages/shadcn-ui/src/runtime/components/shared/Section/SectionContainer.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
 import { Primitive, type PrimitiveProps } from 'radix-vue'
+import { computed } from 'vue'
 
-const props = defineProps<PrimitiveProps>()
+const props = withDefaults(defineProps<{
+    fullWidth?: boolean
+} & PrimitiveProps>(), {
+    fullWidth: false
+})
+
+const primitiveProps = computed(() => {
+    const { fullWidth, ...primitiveProps } = props
+
+    return primitiveProps
+})
 </script>
 
 <template>
     <Primitive
-        class="container"
-        :="props"
+        :class="[
+            'container',
+            { ['max-w-max']: fullWidth }
+        ]"
+        :="primitiveProps"
     >
         <slot></slot>
     </Primitive>
