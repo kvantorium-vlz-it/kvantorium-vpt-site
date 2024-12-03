ae662a8359473285206b69c9f668acd23cb5bd63
Timur <81884993+toombez@users.noreply.github.com>
2024-12-04 01:10:45 +0300

    Feat/footer (#99)
    * Add draft footer
    
    * Add FooterCopyright
    
    * Add Footer Info
    
    * Add Footer Site Navigation
    
    * Add Footer Kvantorium Links
    
    * Change heading text in Footer Kvantorium Links
    
    * Add Footer Polytech Links
    
    * Compose footer

---
+++ packages/shadcn-ui/src/runtime/components/Footer/FooterSiteNavigation.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ArrowUpRightIcon } from 'lucide-vue-next'
+
+const navigation = [
+    { label: 'Главная', to: '/' },
+    { label: 'О нас', to: '/about/' },
+    { label: 'Новости', to: '/news/' },
+    { label: 'Направления', to: '/kvantums/' },
+]
+</script>
+
+<template>
+    <section>
+        <h2 class="font-display text-3xl mb-2 tracking-tighter leading-none">
+            Навигация
+        </h2>
+
+        <ul class="flex flex-col gap-y-0.5">
+            <li v-for="link in navigation">
+                <NuxtLink
+                    class="grid grid-cols-[auto,minmax(auto,1fr)] gap-2 items-center group/footer-link hover:pl-2 transition-all"
+                    :to="link.to"
+                >
+                    <ArrowUpRightIcon
+                        class="group-hover/footer-link:rotate-45 transition-all"
+                        :size="16"
+                    />
+
+                    <span>
+                        {{ link.label }}
+                    </span>
+                </NuxtLink>
+            </li>
+        </ul>
+    </section>
+</template>
---
+++ packages/shadcn-ui/src/runtime/components/Footer/TheFooter.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import FooterCopyright from './FooterCopyright.vue'
+import FooterInfo from './FooterInfo.vue'
+import FooterSiteNavigation from './FooterSiteNavigation.vue'
+import FooterKvantoriumLinks from './FooterKvantoriumLinks.vue'
+import FooterPolytechLinks from './FooterPolytechLinks.vue'
+</script>
+
+<template>
+    <Section>
+        <SectionContainer full-width>
+            <div class="bg-gradient-to-br from-blue-900 to-blue-600 text-white rounded-xl p-8 lg:py-16 lg:px-16 lg:pb-8 relative isolate after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 after:bg-hero-noice after:-z-10 after:opacity-25 after:pointer-events-none">
+                <div class="grid grid-cols-1 grid-flow-row lg:grid-rows-2 lg:grid-cols-4 gap-y-8 lg:gap-2">
+                    <FooterInfo class="lg:col-start-1 lg:row-start-1 lg:row-span-2" />
+                    <FooterSiteNavigation class="lg:col-start-3" />
+                    <FooterKvantoriumLinks class="lg:col-start-4" />
+                    <FooterPolytechLinks class="lg:col-start-4" />
+                </div>
+
+                <hr class="my-8">
+
+                <FooterCopyright />
+            </div>
+        </SectionContainer>
+    </Section>
+</template>
