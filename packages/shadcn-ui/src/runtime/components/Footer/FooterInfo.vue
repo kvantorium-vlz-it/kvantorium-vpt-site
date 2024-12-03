7d9667a52f6ad5f9a7d70148db12276e67f9a9d0
Timur <toombez.timurtokaev@gmail.com>
2024-12-04 00:32:05 +0300

    Add Footer Info

---
+++ packages/shadcn-ui/src/runtime/components/Footer/FooterInfo.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <header class="grid grid-cols-[auto,minmax(auto,1fr)] gap-2 items-center mb-4">
+            <div class="bg-white w-16 p-3.5 rounded-full">
+                <KvantoriumLogo />
+            </div>
+
+            <h2 class="font-display leading-none tracking-tight text-lg">
+                детский технопарк "кванториум "волжский политех"
+            </h2>
+        </header>
+
+        <div>
+            <p class="whitespace-break-spaces text-balance mb-2">
+                Детский технопарк «Кванториум» - это новая форма дополнительного образования, которая реализует научно-технический интерес детей. Это место интеллектуальной смелости и комфортные условия формирования изобретательского мышления.
+            </p>
+
+            <ShButton
+                variant="ghost"
+                class="w-full border-2 border-white"
+            >
+                Записаться
+            </ShButton>
+        </div>
+    </section>
+</template>
