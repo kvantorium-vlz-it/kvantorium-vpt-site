2ab4ae83f0e37dbd51897bbdc8286010080e37c4
Timur <toombez.timurtokaev@gmail.com>
2024-12-04 00:17:22 +0300

    Add FooterCopyright

---
+++ packages/shadcn-ui/src/runtime/components/Footer/FooterCopyright.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear()
+</script>
+
+<template>
+    <div class="text-center font-display text-md leading-tight tracking-tighter">
+        <span>
+            детский технопарк "кванториум "волжский политех"
+        </span>
+
+        <br>
+
+        <span>
+            {{ currentYear }}
+        </span>
+    </div>
+</template>
