4d1c43a8ef0f51a10ff5e893ea122c004b8d106e
Timur <81884993+toombez@users.noreply.github.com>
2024-12-10 15:01:39 +0300

    Feat/kvantum-provider (#103)
    * Use Kvantum interface in KvantumCard
    
    * Extract KvantumsCarousel
    
    * Install nuxt sanity
    
    * Add KvantumProvider
    
    * Add single kvantum provider
    
    * Add info card to kvantums carousel

---
+++ apps/web/components/providers/KvantumsProvider.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { Kvantum } from '~/assets/typescript/types'
+
+const query = groq`*[_type == 'kvantum'] {
+    ...,
+    'slug': slug.current,
+    'icon': icon.image.asset->url
+}`
+
+const {
+    data,
+} = await useSanityQuery<Kvantum[]>(query)
+</script>
+
+<template>
+    <slot :kvantums="data ?? []"></slot>
+</template>
--- apps/web/components/sections/Kvantums/TheKvantumsSection.vue
+++ apps/web/components/sections/Kvantums/TheKvantumsSection.vue
@@ -1,27 +1,11 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { cn } from '@/lib/utils';
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, type CarouselApi } from '@/components/shadcn/carousel'
-import { watchOnce } from '@vueuse/core';
-import Progress from '@/components/shadcn/progress/Progress.vue';
+import type { Kvantum } from '~/assets/typescript/types'
+import { TrendingUpIcon } from 'lucide-vue-next'
 
-const api = ref<CarouselApi>()
-const progress = ref(0)
-
-function setApi(val: CarouselApi) {
-    api.value = val
-}
-
-watchOnce(api, (api) => {
-    if (!api) {
-        return
-    }
-
-    progress.value = api.selectedScrollSnap() / (api.scrollSnapList().length - 1)
-
-    api.on('select', () => {
-        progress.value = api.selectedScrollSnap() / (api.scrollSnapList().length - 1)
-    })
+withDefaults(defineProps<{
+    kvantums: Kvantum[]
+}>(), {
+    kvantums: () => [],
 })
 </script>
 
@@ -32,40 +16,20 @@ watchOnce(api, (api) => {
                 Квантумы
             </SectionHeading>
 
-            <Carousel v-slot="{ canScrollNext, canScrollPrev }" @init-api="setApi">
-                <CarouselContent>
-                    <CarouselItem class="basis-full lg:basis-1/3" v-for="i in 10">
-                        <KvantumCard
-                            :from-age="12"
-                            kvantum-image="https://cdn.sanity.io/images/89inj6dr/production/fe29435a48259d377b7520bc2f0c5598f00cde59-265x265.png"
-                            :kvantum-name="`Lorem ipsum dolor sit amet consectetur.`"
-                            to="/"
-                            :topics="[
-                                'Веб-разработка',
-                                'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                                'Гейм-дев',
-                            ]"
-                        />
-                    </CarouselItem>
-                </CarouselContent>
-
-                <div class="relative grid grid-cols-3 mt-6 gap-y-2">
-                    <Progress :model-value="progress * 100" class="h-1 row-start-1 col-start-2" />
-
-                    <div class="row-start-2 col-start-2 w-fit mx-auto inline-flex gap-2">
-                        <CarouselPrevious
-                            :disabled="!canScrollPrev"
-                            :class="cn(
-                                `top-auto right-auto bottom-auto left-auto translate-x-0 translate-y-0 relative`,
-                            )"
-                        />
-                        <CarouselNext
-                            :disabled="!canScrollNext"
-                            class="top-auto right-auto bottom-auto left-auto translate-x-0 translate-y-0 relative"
-                        />
-                    </div>
-                </div>
-            </Carousel>
+            <KvantumsCarousel :kvantums="kvantums">
+                <template #before-slides>
+                    <ShCarouselItem class="basis-full lg:basis-1/3">
+                        <ShCard class="flex flex-col justify-end items-end from-blue-800 to-blue-600 bg-gradient-to-br text-white h-full">
+                            <ShCardHeader>
+                                <TrendingUpIcon :size="56" />
+                            </ShCardHeader>
+                            <ShCardContent class="text-lg">
+                                Волжский кванториум предлагает множество различных направлений подготовки, которые помогут раскрыть способности ребенка
+                            </ShCardContent>
+                        </ShCard>
+                    </ShCarouselItem>
+                </template>
+            </KvantumsCarousel>
         </SectionContainer>
     </Section>
 </template>
