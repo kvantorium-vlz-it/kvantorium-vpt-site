5973d62bb6b4358cdab339cfdac589a641675dae
Timur <toombez.timurtokaev@gmail.com>
2024-12-15 23:43:46 +0300

    Add types for news

--- apps/web/assets/typescript/types.ts
+++ apps/web/assets/typescript/types.ts
@@ -5,3 +5,24 @@ export interface Kvantum {
     icon: string,
     description: any[]
 }
+
+interface ImageWithAlternativeText {
+    src: string
+    alt: string
+}
+
+interface NewsTag {
+    _id: string
+    name: string
+}
+
+export interface News {
+    publishDate: string
+    tags: NewsTag[]
+    gallery: ImageWithAlternativeText[]
+    title: string
+    slug: string
+    _id: string
+    content: any[]
+    previewImage: ImageWithAlternativeText,
+}
