04a792a1be3a083282a1ee906ed5ee31f168d856
Timur <toombez.timurtokaev@gmail.com>
2024-12-21 17:50:00 +0300

    Add styles to portable block

--- apps/cms/src/schemas/objects/portableText.ts
+++ apps/cms/src/schemas/objects/portableText.ts
@@ -1,5 +1,5 @@
-import { defineType } from "sanity";
-import { OBJECT_TYPES } from "../../constants";
+import { defineType } from "sanity"
+import { OBJECT_TYPES } from "../../constants"
 
 export default defineType({
     name: OBJECT_TYPES.PORTABLE_TEXT,
@@ -7,6 +7,15 @@ export default defineType({
     of: [
         {
             type: 'block',
+            styles: [
+                { title: 'Обычный', value: 'normal' },
+                { title: 'Заголовок 1', value: 'h1' },
+                { title: 'Заголовок 2', value: 'h2' },
+                { title: 'Заголовок 3', value: 'h3' },
+                { title: 'Заголовок 4', value: 'h4' },
+                { title: 'Заголовок 5', value: 'h5' },
+                { title: 'Заголовок 6', value: 'h6' },
+            ],
         },
     ],
 })
