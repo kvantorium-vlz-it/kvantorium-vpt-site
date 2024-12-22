3d3d6f8de351af6a868559d24659ab7aba295413
Timur <toombez.timurtokaev@gmail.com>
2024-12-22 17:01:32 +0300

    Add em, strong, underline marks

---
+++ apps/web/components/portable-text/marks/EmMark.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { PortableTextMarkComponentProps } from '@portabletext/vue'
+
+defineProps<PortableTextMarkComponentProps>()
+</script>
+
+<template>
+    <em class="italic">
+        <slot></slot>
+    </em>
+</template>
