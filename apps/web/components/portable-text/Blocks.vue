6b2d3c218096a418f24909f8d14d5d31418c204c
Timur <toombez.timurtokaev@gmail.com>
2024-12-24 15:53:42 +0300

    Add PortableBlock

---
+++ apps/web/components/portable-text/Blocks.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { PortableText } from '@portabletext/vue'
+import StrongMark from './marks/StrongMark.vue'
+import UnderlineMark from './marks/UnderlineMark.vue'
+import EmMark from './marks/EmMark.vue'
+import ImageBlock from './blocks/ImageBlock.vue'
+import NormalBlock from './blocks/NormalBlock.vue'
+import HeadingBlock from './blocks/HeadingBlock.vue'
+import LinkMark from './marks/LinkMark.vue'
+
+defineProps<{
+    blocks: any[]
+}>()
+</script>
+
+<template>
+    <PortableText
+        :value="blocks"
+        :components="{
+            block: {
+                normal: NormalBlock,
+                h1: HeadingBlock,
+                h2: HeadingBlock,
+                h3: HeadingBlock,
+                h4: HeadingBlock,
+                h5: HeadingBlock,
+                h6: HeadingBlock,
+            },
+
+            types: {
+                image: ImageBlock,
+            },
+
+            marks: {
+                em: EmMark,
+                strong: StrongMark,
+                underline: UnderlineMark,
+                link: LinkMark,
+            },
+        }"
+        :on-missing-component="false"
+    />
+</template>
