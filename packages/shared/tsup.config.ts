92ef4184429300364a12c3532c8ae314a39caafd
Timur <toombez.timurtokaev@gmail.com>
2024-12-25 01:02:22 +0300

    Setup entries

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -4,12 +4,15 @@ export default defineConfig((options) => {
     return {
         entry: [
             'src/index.ts',
+            'src/queries/index.ts',
+            'src/schemas/index.ts',
+            'src/constants.ts',
         ],
         outDir: 'dist',
         clean: !options.watch,
         dts: true,
         minify: !options.watch,
-        format: [/*'cjs',*/ 'esm'],
+        format: ['cjs', 'esm'],
         sourcemap: !!options.watch,
         treeshake: !options.watch,
     }
