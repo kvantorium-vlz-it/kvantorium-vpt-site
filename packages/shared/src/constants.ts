c51be27b18f0f899596e2ad4d762e988fb296b6a
Timur <toombez.timurtokaev@gmail.com>
2024-12-25 01:02:46 +0300

    Add misc constants

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -13,3 +13,14 @@ export const DOCUMENT_TYPES = arrayToConstants([
 export const OBJECT_TYPES = arrayToConstants([
     'portableText',
 ] as const, SCHEMA_PREFIX)
+
+export const CURRICULUM_LEVEL = {
+    INTRODUCTORY: 0,
+    ADVANCED: 1,
+    PROJECT: 2,
+} as const
+
+export const LINK_TYPE = {
+    EXTERNAL: 0,
+    INTERNAL: 1,
+} as const
