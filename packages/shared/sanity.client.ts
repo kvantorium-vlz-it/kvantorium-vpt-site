0b9c4326d203ff43ea4aa27c5e3b6549929bec24
Timur <toombez.timurtokaev@gmail.com>
2025-01-05 23:39:50 +0300

    Add queries and schema to typegen

--- packages/shared/sanity.client.ts
+++ packages/shared/sanity.client.ts
@@ -1,6 +1,55 @@
 import { createClient } from "@sanity/client"
+import groq from "groq"
 
-const client = createClient({
+export const client = createClient({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
     dataset: process.env.SANITY_STUDIO_DATASET!,
 })
+
+const portableTextQuery = groq`*[_type == 'kvantorium.kvantum'] {
+    'description': description[]{
+        _key,
+        _type == 'block' => {
+            _type,
+            level,
+            children[] {
+                marks,
+                text,
+                _type,
+                _key,
+            },
+            style,
+            markDefs {
+                _type,
+                _key,
+            }
+        },
+        _type == 'image' => {
+            crop {
+                top,
+                right,
+                bottom,
+                left,
+            },
+            _type,
+            'asset': asset-> {
+                _type,
+                _id,
+                title,
+                description,
+                'alt': altText,
+                'src': url,
+                'dimensions': metadata.dimensions {
+                    aspectRatio,
+                    height,
+                    width,
+                },
+            },
+        },
+    }
+}`
+
+async () => {
+    const result = await client.fetch(portableTextQuery)
+    return result
+}
--- packages/shared/sanity.config.ts
+++ packages/shared/sanity.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "sanity"
+import {
+    curriculumSchema,
+    employeeSchema,
+    kvantumSchema,
+    newsSchema,
+    newsTagSchema,
+    portableTextSchema,
+} from './src/schemas/index.ts'
 
 export default defineConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
     dataset: process.env.SANITY_STUDIO_DATASET!,
     schema: {
         types: [
+            portableTextSchema,
+            newsTagSchema,
+            employeeSchema,
+            kvantumSchema,
+            curriculumSchema,
+            newsSchema,
         ]
     },
 })
