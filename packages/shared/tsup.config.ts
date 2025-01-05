4d97b8ec1a1ebf719dac32f130b8444a658467ff
Timur <toombez.timurtokaev@gmail.com>
2025-01-05 23:36:23 +0300

    Setup tsconfig and tsup

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,19 +1,23 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig((options) => {
-    return {
-        entry: [
-            'src/index.ts',
-            'src/queries/index.ts',
-            'src/schemas/index.ts',
-            'src/constants.ts',
-        ],
-        outDir: 'dist',
-        clean: !options.watch,
-        dts: true,
-        minify: !options.watch,
-        format: ['cjs', 'esm'],
-        sourcemap: !!options.watch,
-        treeshake: !options.watch,
-    }
-})
+export default defineConfig((options) => ({
+    entry: [
+        // './src/.shared/schema.json',
+        // './src/.shared/sanity.types.ts',
+        './src/constants.ts',
+        './src/utils.ts',
+        './src/groq/groqd.client.ts',
+        './src/groq/fragments/index.ts',
+        './src/groq/queries/index.ts',
+        './src/groq/index.ts',
+        './src/schemas/index.ts',
+        './src/index.ts',
+    ],
+    publicDir: "./src/.shared",
+    dts: true,
+    minify: !!!options.watch,
+    outDir: 'dist',
+    sourcemap: !!options.watch,
+    clean: !!!options.watch,
+    format: ['cjs', 'esm'],
+}))
