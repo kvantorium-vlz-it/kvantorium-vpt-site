3ff38adcc2db4c93d7c2e77446dea4e107a79fa9
Timur <81884993+toombez@users.noreply.github.com>
2025-01-13 12:45:34 +0300

    Feat/kvantums-page (#108)
    * build schema before dev in shared
    
    * Add all kvantums page
    
    * Change specific kvantum page working on mobile

---
+++ apps/web/pages/kvantums/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Section>
+            <SectionContainer>
+                <SectionHeading>
+                    Квантумы
+                </SectionHeading>
+
+                <ul class="grid grid-cols-1 gap-2 lg:grid-cols-3">
+                    <KvantumListProvider #="{ kvantums }">
+                        <li v-for="kvantum in kvantums" :key="kvantum._id">
+                            <KvantumCard :kvantum="kvantum" />
+                        </li>
+                    </KvantumListProvider>
+                </ul>
+            </SectionContainer>
+        </Section>
+    </div>
+</template>
