f125f5653147e599439cdc2b54c8fd18474539a5
Timur <toombez.timurtokaev@gmail.com>
2025-01-24 01:22:06 +0300

    Comment constantsFromArray util

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,18 +1,16 @@
-import { constantsFromArray } from "./utils"
-
 const SCHEMA_PREFIX = 'kvantorium' as const
 
-export const DOCUMENT_TYPES = constantsFromArray([
-    'portableText',
-    'link',
-    'internalLink',
-    'externalLink',
-    'newsTag',
-    'employee',
-    'kvantum',
-    'curriculum',
-    'news',
-] as const, [SCHEMA_PREFIX] as const)
+export const DOCUMENT_TYPES = {
+    PORTABLE_TEXT: `${SCHEMA_PREFIX}.portableText`,
+    LINK: `${SCHEMA_PREFIX}.link`,
+    INTERNAL_LINK: `${SCHEMA_PREFIX}.internalLink`,
+    EXTERNAL_LINK: `${SCHEMA_PREFIX}.externalLink`,
+    NEWS_TAG: `${SCHEMA_PREFIX}.newsTag`,
+    EMPLOYEE: `${SCHEMA_PREFIX}.employee`,
+    KVANTUM: `${SCHEMA_PREFIX}.kvantum`,
+    CURRICULUM: `${SCHEMA_PREFIX}.curriculum`,
+    NEWS: `${SCHEMA_PREFIX}.news`,
+} as const
 
 export const CURRICULUM_LEVEL = {
     INTRODUCTORY: 0,
--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -1,5 +1,4 @@
 import { CURRICULUM_LEVEL } from "@constants"
-import { JoinByCase, snakeCase, SnakeCase } from "scule"
 
 export const getCurriculumLevelLabel = (
     level: typeof CURRICULUM_LEVEL[keyof typeof CURRICULUM_LEVEL]
@@ -13,35 +12,36 @@ export const getCurriculumLevelLabel = (
     }
 }
 
+// Bug:Break import on @kvantoriumvlz/cms
 
-export const constantsFromArray = <
-    C extends ReadonlyArray<string>,
-    P extends ReadonlyArray<string> = [],
-    S extends string = '.'
->(constants: C, prefixes?: P, separator?: S) => {
-    const joinedPrefixes = (prefixes || []).join(separator || '.')
-    const isPrefixesDefined = (prefixes || []).length > 0
+// export const constantsFromArray = <
+//     C extends ReadonlyArray<string>,
+//     P extends ReadonlyArray<string> = [],
+//     S extends string = '.'
+// >(constants: C, prefixes?: P, separator?: S) => {
+//     const joinedPrefixes = (prefixes || []).join(separator || '.')
+//     const isPrefixesDefined = (prefixes || []).length > 0
 
-    return constants.reduce(
-        (constants, constant) => ({
-            ...constants,
-            [snakeCase(constant).toUpperCase()]: (isPrefixesDefined
-                ? `${joinedPrefixes}${separator || '.'}${constant}`
-                : constant
-            )
-        }),
-        {}
-    ) as ConstantsFromArray<C, P, S>
-}
+//     return constants.reduce(
+//         (constants, constant) => ({
+//             ...constants,
+//             [snakeCase(constant).toUpperCase()]: (isPrefixesDefined
+//                 ? `${joinedPrefixes}${separator || '.'}${constant}`
+//                 : constant
+//             )
+//         }),
+//         {}
+//     ) as ConstantsFromArray<C, P, S>
+// }
 
-export type ConstantsFromArray<
-    C extends ReadonlyArray<string>,
-    P extends ReadonlyArray<string>,
-    S extends string = '.'
-> = {
-    [K in C[number] as Uppercase<SnakeCase<K>>]: (
-        JoinByCase<P, S> extends ""
-            ? K
-            : `${JoinByCase<P, S>}${S}${K}`
-    )
-}
+// export type ConstantsFromArray<
+//     C extends ReadonlyArray<string>,
+//     P extends ReadonlyArray<string>,
+//     S extends string = '.'
+// > = {
+//     [K in C[number] as Uppercase<SnakeCase<K>>]: (
+//         JoinByCase<P, S> extends ""
+//             ? K
+//             : `${JoinByCase<P, S>}${S}${K}`
+//     )
+// }
