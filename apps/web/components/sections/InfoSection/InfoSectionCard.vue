78eba750f8895cc1d3cc8d8dd805423a0997b397
Timur <toombez.timurtokaev@gmail.com>
2025-01-30 21:24:42 +0300

    Add InfoSectionCard

---
+++ apps/web/components/sections/InfoSection/InfoSectionCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <ShCard class="">
+        <ShCardHeader>
+            <ShCardTitle class="font-display text-center text-3xl">
+                <slot name="title"></slot>
+            </ShCardTitle>
+        </ShCardHeader>
+        <ShCardContent>
+            <slot></slot>
+        </ShCardContent>
+    </ShCard>
+</template>
