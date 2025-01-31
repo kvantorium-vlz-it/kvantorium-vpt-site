c9598eb035be1f75f21ee90c1d62e0aa265917ca
Timur <toombez.timurtokaev@gmail.com>
2025-01-31 13:19:18 +0300

    Query kvantum in single kvantum page

--- apps/web/pages/kvantums/[slug].vue
+++ apps/web/pages/kvantums/[slug].vue
@@ -1,46 +1,46 @@
 <script setup lang="ts">
+import { kvantumFragmentFactory } from '@kvantoriumvlz/query';
+import { DOCUMENT_TYPES } from '@kvantoriumvlz/shared';
+import { q } from '~/assets/typescript/groqd.client';
+import type { Kvantum } from '~/assets/typescript/types';
+
 const route = useRoute()
 
 const slug = route.params.slug as string
+
+const query = q
+    .star
+    .filterByType(DOCUMENT_TYPES.KVANTUM)
+    .filterBy(`slug.current == "${slug}"`)
+    .project(kvantumFragmentFactory(q))
+    .slice(0)
+
+const { data: kvantum } = await useSanityQuery<Kvantum>(query.query)
 </script>
 
 <template>
-    <KvantumProvider #="{ kvantum }" :slug="slug">
-        <Section>
-            <SectionContainer>
-                <SectionHeading>
-                    <span v-if="kvantum">
-                        {{ kvantum.name }}
-                    </span>
-                    <span v-else>
-                        Квантум не найден
-                    </span>
-                </SectionHeading>
+    <Section>
+        <SectionContainer>
+            <SectionHeading>
+                <span v-if="kvantum">
+                    {{ kvantum.name }}
+                </span>
+                <span v-else>
+                    Квантум не найден
+                </span>
+            </SectionHeading>
 
-                <div v-if="kvantum" class="grid grid-cols-1 lg:grid-cols-[auto,minmax(auto,1fr)] gap-4">
-                    <img
-                        class="block mx-auto max-w-64 w-full lg:sticky lg:top-32"
-                        :src="kvantum.icon?.asset?.src!"
-                        :alt="`Логотип ${kvantum.name}`"
-                    >
+            <div v-if="kvantum" class="grid grid-cols-1 lg:grid-cols-[auto,minmax(auto,1fr)] gap-4">
+                <img
+                    class="block mx-auto max-w-64 w-full lg:sticky lg:top-32"
+                    :src="kvantum.icon?.asset?.src!"
+                    :alt="`Логотип ${kvantum.name}`"
+                >
 
-                    <div class="mt-4">
-                        <PortableBlocks :blocks="kvantum.description" />
-                    </div>
+                <div class="mt-4">
+                    <PortableBlocks :blocks="kvantum.description" />
                 </div>
-            </SectionContainer>
-        </Section>
-
-        <Section>
-            <SectionContainer>
-                <SectionHeading>
-                    Все квантумы
-                </SectionHeading>
-
-                <KvantumListProvider #="{ kvantums }">
-                    <KvantumsCarousel :kvantums="kvantums" />
-                </KvantumListProvider>
-            </SectionContainer>
-        </Section>
-    </KvantumProvider>
+            </div>
+        </SectionContainer>
+    </Section>
 </template>
