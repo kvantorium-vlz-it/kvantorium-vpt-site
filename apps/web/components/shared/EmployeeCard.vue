b13b59a6e37a9bc7f8570bffb64df965ba402403
Timur <toombez.timurtokaev@gmail.com>
2025-02-08 00:05:03 +0300

    Add employee card

---
+++ apps/web/components/shared/EmployeeCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { Employee } from '~/assets/typescript/types'
+
+const props = defineProps<{
+    employee: Employee
+}>()
+
+const fullName = computed(() => `${props.employee.surname} ${props.employee.name} ${props.employee.patronymic}`)
+</script>
+
+<template>
+    <ShCard>
+        <ShCardHeader>
+            <ShCardTitle>
+                {{ fullName }}
+            </ShCardTitle>
+
+            <ShCardDescription>
+                {{ employee.description }}
+            </ShCardDescription>
+        </ShCardHeader>
+
+        <ShCardContent>
+            <img
+                :src="employee.image.asset.src!"
+                alt=""
+                class="aspect-square object-cover rounded-lg"
+            >
+        </ShCardContent>
+    </ShCard>
+</template>
