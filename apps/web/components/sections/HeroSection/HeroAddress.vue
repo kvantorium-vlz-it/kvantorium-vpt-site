0775a3e5fe12ec33a36deceed84c5528dd855f9c
Timur <toombez.timurtokaev@gmail.com>
2025-02-12 13:42:50 +0300

    Change sizes

--- apps/web/components/sections/HeroSection/HeroAddress.vue
+++ apps/web/components/sections/HeroSection/HeroAddress.vue
@@ -7,7 +7,7 @@ import { MapPinIcon } from 'lucide-vue-next'
         class="
             w-fit mx-auto
             font-serif text-center not-italic text-white
-            text-body-extra-small tablet:text-heading-5 laptop:text-heading-4 desktop:text-heading-3
+            text-body-extra-small tablet:text-heading-5 laptop:text-heading-4 desktop:text-heading-4
             leading-heading -tracking-[0.05ch]
             group relative
         "
@@ -16,12 +16,12 @@ import { MapPinIcon } from 'lucide-vue-next'
             href="geo:48.786934,44.772160"
             class="flex justify-center items-baseline gap-2"
         >
-            <MapPinIcon class="size-3 tablet:size-5 laptop:size-6 desktop:size-8" />
+            <MapPinIcon class="size-3 tablet:size-5 laptop:size-6 desktop:size-6" />
 
             <span
                 class="
-                    after:h-1 after:rounded-3xl
-                    after:absolute after:-bottom-2 after:left-1/2 after:right-1/2
+                    after:h-0.5 laptop:after:h-1 after:rounded-3xl
+                    after:absolute after:-bottom-2 after:left-full after:right-full
                     after:bg-background
                     after:transition-all
                     group-hover:after:left-0 group-hover:after:right-0
