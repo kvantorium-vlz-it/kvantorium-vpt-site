7c3cd1390ea5361c90024002d88b8f7d03f775a5
Timur <toombez.timurtokaev@gmail.com>
2025-02-17 15:17:23 +0300

    Header navigation menu items

---
+++ apps/web/components/sections/Header/HeaderNavigationMenuTrigger.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import type {
+    ShadcnNavigationMenuTriggerProps
+} from '~/components/shadcn/navigation-menu/NavigationMenuTrigger.vue'
+
+export interface HeaderNavigationMenuTriggerProps {
+    color?: ShadcnNavigationMenuTriggerProps['color']
+    size?: ShadcnNavigationMenuTriggerProps['size']
+}
+</script>
+
+<script setup lang="ts">
+defineProps<HeaderNavigationMenuTriggerProps>()
+</script>
+
+<template>
+    <ShNavigationMenuTrigger
+        :color="color"
+        :size="size"
+    >
+        <slot></slot>
+    </ShNavigationMenuTrigger>
+</template>
