f6a1dd35fa83b3fdb5831fd1f26dc25044953a86
Timur <toombez.timurtokaev@gmail.com>
2025-02-17 11:16:39 +0300

    Use BaseLink in shadcn navigation menu trigger

--- apps/web/components/shadcn/navigation-menu/NavigationMenuTrigger.vue
+++ apps/web/components/shadcn/navigation-menu/NavigationMenuTrigger.vue
@@ -1,18 +1,24 @@
-<script setup lang="ts">
+<script lang="ts">
 import { cn } from '@/lib/utils'
-import { ChevronDown } from 'lucide-vue-next'
-import {
-  NavigationMenuTrigger,
-  type NavigationMenuTriggerProps,
-  useForwardProps,
-} from 'radix-vue'
-import { computed, type HTMLAttributes } from 'vue'
-import { navigationMenuTriggerStyle } from '.'
+import { ChevronDownIcon } from 'lucide-vue-next'
+import { NavigationMenuTrigger, useForwardProps } from 'radix-vue'
+import type { NavigationMenuTriggerProps } from 'radix-vue'
+import type { HTMLAttributes } from 'vue'
+import type { BaseLinkProps } from '~/components/shared/Navigation/BaseLink.vue'
 
-const props = defineProps<NavigationMenuTriggerProps & { class?: HTMLAttributes['class'] }>()
+export type ShNavigationMenuTriggerProps = NavigationMenuTriggerProps & {
+    class?: HTMLAttributes['class']
+    color?: BaseLinkProps['color']
+    size?: BaseLinkProps['size']
+}
+
+</script>
+
+<script setup lang="ts">
+const props = defineProps<ShNavigationMenuTriggerProps>()
 
 const delegatedProps = computed(() => {
-    const { class: _, ...delegated } = props
+    const { class: _, size, color, ...delegated } = props
 
     return delegated
 })
@@ -21,14 +27,22 @@ const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
-    <NavigationMenuTrigger
-        v-bind="forwardedProps"
-        :class="cn(navigationMenuTriggerStyle(), 'group', props.class)"
+    <BaseLink
+        as-child
+        variant="outline"
+        :color="color"
+        :size="size"
     >
-        <slot />
-        <ChevronDown
-            class="size-5 transition duration-200 group-data-[state=open]:rotate-180"
-            aria-hidden="true"
-        />
-    </NavigationMenuTrigger>
+        <NavigationMenuTrigger
+            v-bind="forwardedProps"
+            :class="cn('group', props.class)"
+        >
+            <slot />
+
+            <ChevronDownIcon
+                class="transition duration-200 group-data-[state=open]:rotate-180"
+                aria-hidden="true"
+            />
+        </NavigationMenuTrigger>
+    </BaseLink>
 </template>
--- apps/web/components/shadcn/navigation-menu/index.ts
+++ apps/web/components/shadcn/navigation-menu/index.ts
@@ -8,14 +8,14 @@ export { default as NavigationMenuList } from './NavigationMenuList.vue'
 export { default as NavigationMenuTrigger } from './NavigationMenuTrigger.vue'
 
 export const navigationMenuTriggerStyle = cva(
-    `
-    group
-    transition-colors
-    inline-flex items-center justify-center gap-1
-    font-light text-lg text-white font-body leading-input
-    disabled:opacity-50 disabled:pointer-events-none
-    hover:text-gray-light focus:text-gray-light data-[state=open]:text-gray-light
-    `,
+    // `
+    // group
+    // transition-colors
+    // inline-flex items-center justify-center gap-1
+    // font-light text-lg text-white font-body leading-input
+    // disabled:opacity-50 disabled:pointer-events-none
+    // hover:text-gray-light focus:text-gray-light data-[state=open]:text-gray-light
+    // `,
 )
 
 // hover:bg-gray-light/15 hover:text-accent-foreground
