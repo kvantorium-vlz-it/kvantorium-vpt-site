ca79338489697f8a5033a2c4f22b4560bd722d83
Timur <toombez.timurtokaev@gmail.com>
2025-02-18 13:46:06 +0300

    Fix svg for badge

--- apps/web/components/shadcn/badge/Badge.vue
+++ apps/web/components/shadcn/badge/Badge.vue
@@ -10,7 +10,7 @@ export const badgeVariants = cva(
     rounded-full border
     font-serif leading-input -tracking-base
     transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2
-    [&_svg]:pointer-events-none [&_svg]:shrink-0
+    [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:transition-transform
     `,
     {
         variants: {
@@ -26,9 +26,9 @@ export const badgeVariants = cva(
                 outline: 'border-gray',
             },
             size: {
-                small: 'px-1.5 py-0.5 text-sm font-medium [&>svg]:size-3 gap-1',
-                default: 'px-2.5 py-1 text-base font-medium [&>svg]:size-4 gap-1.5',
-                big: 'px-6 py-1.5 text-md font-semibold [&>svg]:size-5 gap-2',
+                small: 'px-1.5 py-0.5 text-sm font-medium [&_svg]:size-3 gap-1',
+                default: 'px-2.5 py-1 text-base font-medium [&_svg]:size-4 gap-1.5',
+                big: 'px-6 py-1.5 text-md font-semibold [&_svg]:size-5 gap-2',
             }
         },
         compoundVariants: [
