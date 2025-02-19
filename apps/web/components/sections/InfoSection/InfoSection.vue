c4e3c80b1e91721dd3ff2b5ac5f23065f406123e
Timur <toombez.timurtokaev@gmail.com>
2025-02-20 02:17:12 +0300

    Use Timeline in InfoSection

--- apps/web/components/sections/InfoSection/InfoSection.vue
+++ apps/web/components/sections/InfoSection/InfoSection.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+const data = [
+    {
+        id: 1,
+        title: "О нас",
+        content: 'Детские технопарки «Кванториум» — это федеральная сеть образовательных площадок, оснащенных высокотехнологичным оборудованием, где дети учатся по принципу проектного обучения: от теории сразу к практике.',
+    },
+    {
+        id: 2,
+        title: 'Миссия',
+        content: 'Содействовать ускоренному техническому развитию детей и реализации научно-технического потенциала российской молодежи, внедряя эффективные модели образования, доступные для тиражирования во всех регионах страны.',
+    },
+    {
+        id: 3,
+        title: 'Задачи',
+        items: [
+            'Развитие творческого потенциала детей',
+            'Возрождение интереса к техническим профессиям',
+            'Воспитание будущих высококлассных специалистов в стратегически важных областях российской науки и техники',
+        ],
+    }
+];
+</script>
+
 <template>
     <Section>
         <SectionHeading>
@@ -5,45 +29,37 @@
         </SectionHeading>
 
         <SectionContainer>
-            <ul class="grid grid-cols-1 lg:grid-cols-3 gap-4 grid-flow-row">
-                <li>
-                    <InfoSectionCard>
-                        <template #title>
-                            О нас
-                        </template>
+            <div class="max-w-[100ch] mx-auto">
 
-                        Детские технопарки «Кванториум» — это федеральная сеть образовательных площадок, оснащенных высокотехнологичным оборудованием, где дети учатся по принципу проектного обучения: от теории сразу к практике.
-                    </InfoSectionCard>
-                </li>
-                <li>
-                    <InfoSectionCard>
-                        <template #title>
-                            Миссия
-                        </template>
+            <Timeline :items="data" >
+                <template
+                    v-for="(item, index) in data"
+                    :key="item.id + index"
+                    #[item.id]
+                >
+                    <div
+                        class="relative w-full pl-20 pr-4"
+                        v-motion-slide-visible-once-right
+                    >
+                        <h3 class="text-3xl text-foreground -tracking-tight font-serif">
+                            {{ item.title }}
+                        </h3>
 
-                        Содействовать ускоренному техническому развитию детей и реализации научно-технического потенциала российской молодежи, внедряя эффективные модели образования, доступные для тиражирования во всех регионах страны.
-                    </InfoSectionCard>
-                </li>
-                <li>
-                    <InfoSectionCard>
-                        <template #title>
-                            Задачи
-                        </template>
+                        <div class="font-serif text-gray-dark text-md leading-body -tracking-tight">
+                            <p v-if="!item.items">
+                                {{ item.content }}
+                            </p>
 
-                        <ol class="list-decimal list-inside">
-                            <li>
-                                Развитие творческого потенциала детей
-                            </li>
-                            <li>
-                                Возрождение интереса к техническим профессиям.
-                            </li>
-                            <li>
-                                Воспитание будущих высококлассных специалистов в стратегически важных областях российской науки и техники
-                            </li>
-                        </ol>
-                    </InfoSectionCard>
-                </li>
-            </ul>
+                            <ol v-else class="list-decimal pl-[1.5ch]">
+                                <li v-for="i in item.items">
+                                    {{ i }}
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </template>
+            </Timeline>
+            </div>
         </SectionContainer>
     </Section>
 </template>
