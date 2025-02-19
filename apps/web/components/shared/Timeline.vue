039048a219d21e56eabac1e159bbab752ddd5392
Timur <toombez.timurtokaev@gmail.com>
2025-02-19 22:39:49 +0300

    Inspira timelime

---
+++ apps/web/components/shared/Timeline.vue
@@ -0,0 +1,101 @@
+<template>
+    <div
+        ref="timelineContainerRef"
+        class="w-full bg-white font-serif md:px-10"
+    >
+        <!-- <div class="mx-auto max-w-7xl px-4 py-20 lg:px-10 md:px-8">
+            <h2 class="mb-4 max-w-4xl text-lg text-black md:text-4xl dark:text-white">
+                {{ title }}
+            </h2>
+
+            <p class="max-w-sm text-sm text-gray md:text-base dark:text-gray-light">
+                {{ description }}
+            </p>
+        </div> -->
+
+        <div
+            ref="timelineRef"
+            class="relative mx-auto max-w-7xl pb-20"
+        >
+            <div
+                v-for="(item, index) in props.items"
+                :key="index"
+                class="flex justify-start pt-10 md:gap-10 md:pt-40"
+            >
+                <div
+                    className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full"
+                >
+                    <div
+                    className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center"
+                    >
+                    <div
+                        className="h-4 w-4 rounded-full bg-blue dark:bg-blue-dark border border-blue-light dark:border-white p-2"
+                    />
+                    </div>
+                    <h3
+                        className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 "
+                    >
+                    </h3>
+                </div>
+
+                <slot :name="item.id"></slot>
+            </div>
+
+            <div
+                :style="{
+                    height: height + 'px',
+                }"
+                class="absolute left-8 top-0 w-[2px] overflow-hidden bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-0% via-blue-dark to-transparent to-[99%] [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] md:left-8 dark:via-neutral-700"
+            >
+                <Motion
+                    as="div"
+                    :style="{
+                        height: heightTransform,
+                        opacity: opacityTransform,
+                    }"
+                    class="absolute inset-x-0 top-0 w-[2px] rounded-full bg-gradient-to-t from-purple-500 from-0% via-blue via-10% to-transparent"
+                >
+                </Motion>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup generic="T extends { id: number }">
+import { Motion, useScroll, useTransform } from "motion-v";
+import type { HTMLAttributes } from "vue";
+
+interface Props {
+    containerClass?: HTMLAttributes["class"]
+    class?: HTMLAttributes["class"]
+    items: T[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    items: () => [],
+});
+
+const timelineContainerRef = ref<HTMLElement | null>(null);
+const timelineRef = ref<HTMLElement | null>(null);
+const height = ref(0);
+
+onMounted(async () => {
+    await nextTick();
+    if (timelineRef.value) {
+        const rect = timelineRef.value.getBoundingClientRect();
+          height.value = rect.height;
+    }
+});
+
+const { scrollYProgress } = useScroll({
+    target: timelineRef,
+    offset: ["start 10%", "end 50%"],
+});
+
+const opacityTransform = ref(useTransform(scrollYProgress, [0, 0.1], [0, 1]));
+const heightTransform = ref(useTransform(scrollYProgress, [0, 1], [0, 0]));
+
+watch(height, (newHeight) => {
+    heightTransform.value = useTransform(scrollYProgress, [0, 1], [0, newHeight]);
+});
+</script>
