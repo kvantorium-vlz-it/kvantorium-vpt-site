6bf6ea6bffe0237762d1f4ae1788b2561674a934
Timur <toombez.timurtokaev@gmail.com>
2025-02-25 14:04:30 +0300

    Add CurriculaList

---
+++ apps/web/components/shared/CurriculaList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { CurriculumCardProps } from './CurriculumCard.vue'
+
+withDefaults(defineProps<{
+    curricula?: CurriculumCardProps['curriculum'][]
+}>(), {
+    curricula: () => [],
+})
+</script>
+
+<template>
+    <ul class="grid grid-cols-1 laptop:grid-cols-2 desktop:grid-cols-3 gap-2">
+        <li
+            v-for="curriculum in curricula"
+            :key="curriculum._id"
+        >
+            <CurriculumCard
+                :curriculum="curriculum"
+                class="h-full"
+            />
+        </li>
+    </ul>
+</template>
