1ff3531f93e38df3dfd0cf8d7a7dc35f0146db09
Timur <toombez.timurtokaev@gmail.com>
2025-02-26 12:12:57 +0300

    Make kvantum icon left and kvantum content right

--- apps/web/pages/kvantums/[slug].vue
+++ apps/web/pages/kvantums/[slug].vue
@@ -29,7 +29,7 @@ const { data: allKvantums } = await useSanityQuery<Kvantum[]>(allKvantumsQuery.q
     <Section>
         <SectionContainer>
             <SectionHeading>
-                <span v-if="kvantum">
+                <span v-if="kvantum" class="text-5xl">
                     {{ kvantum.name }}
                 </span>
                 <span v-else>
@@ -37,14 +37,14 @@ const { data: allKvantums } = await useSanityQuery<Kvantum[]>(allKvantumsQuery.q
                 </span>
             </SectionHeading>
 
-            <div v-if="kvantum" class="grid grid-cols-1 lg:grid-cols-[auto,minmax(auto,1fr)] gap-4">
+            <div v-if="kvantum" class="grid grid-cols-[auto_1fr] gap-4">
                 <img
-                    class="block mx-auto max-w-64 w-full lg:sticky lg:top-32"
+                    class="block mx-auto max-w-64 w-full lg:sticky lg:top-32 sticky top-12"
                     :src="kvantum.icon?.asset?.src!"
                     :alt="`Логотип ${kvantum.name}`"
                 >
 
-                <div class="mt-4">
+                <div class="mt-4 mx-auto">
                     <PortableBlocks :blocks="kvantum.description" />
                 </div>
             </div>
@@ -54,7 +54,40 @@ const { data: allKvantums } = await useSanityQuery<Kvantum[]>(allKvantumsQuery.q
     <Section>
         <SectionContainer>
             <SectionHeading>
-                Все квантумы
+                Учебные программы
+            </SectionHeading>
+
+            <CurriculumListProvider
+                #="{ curricula }"
+                :kvantum="kvantum!"
+                :fetch-per-page="100"
+            >
+                <CurriculaList
+                    :curricula="curricula.map((curriculum) => ({
+                            _id: curriculum._id,
+                            _type: curriculum._type,
+                            hoursPerYear: curriculum.hoursPerYear,
+                            kvantum: {
+                                _id: curriculum.kvantum._id,
+                                _type: curriculum.kvantum._type,
+                                icon: curriculum.kvantum.icon.asset.src!,
+                                name: curriculum.kvantum.name,
+                                slug: curriculum.kvantum.slug,
+                            },
+                            level: curriculum.level,
+                            minimalAge: curriculum.minimalAge,
+                            name: curriculum.name,
+                            type: curriculum._type,
+                    }))"
+                />
+            </CurriculumListProvider>
+        </SectionContainer>
+    </Section>
+
+    <Section>
+        <SectionContainer>
+            <SectionHeading>
+                Другие квантумы
             </SectionHeading>
 
             <KvantumsCarousel :kvantums="allKvantums || []" />
