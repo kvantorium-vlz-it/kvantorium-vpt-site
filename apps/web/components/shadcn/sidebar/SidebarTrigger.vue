3d10baa352fe6397d03c28be219ec1c4fd5c89c2
Timur <toombez.timurtokaev@gmail.com>
2025-03-27 12:09:48 +0300

    Add shadcn sidebar

---
+++ apps/web/components/shadcn/sidebar/SidebarTrigger.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { Button } from '@/components/shadcn/button'
+import { cn } from '@/assets/typescript/shadcn/utils'
+import { PanelLeft } from 'lucide-vue-next'
+import { useSidebar } from './utils'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+
+const { toggleSidebar } = useSidebar()
+</script>
+
+<template>
+    <Button
+        data-sidebar="trigger"
+        variant="ghost"
+        size="small"
+        :class="cn('h-7 w-7', props.class)"
+        @click="toggleSidebar"
+    >
+        <slot>
+            <PanelLeft />
+        </slot>
+        <span class="sr-only">Toggle Sidebar</span>
+    </Button>
+</template>
